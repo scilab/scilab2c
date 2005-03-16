df4fb8ed5545094b2a055794788409050f34db6e
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2005-03-16 01:55:45 +0000

    Better floatvs. double management. Enable Psycho warnings.

--- trunk/scilab2c/src/auxiliaryFunctions/includes/sign.h
+++ trunk/scilab2c/src/auxiliaryFunctions/includes/sign.h
@@ -20,23 +20,23 @@
 
 /**
  ** \brief Float Signe function
- ** Determine the sine of in (assume that 0 is positive).
+ ** Determine the sign of in (assume that 0 is positive).
  ** \param in : the float we must determine sign.
  ** \return -1 or +1 depending on the sign of in.
  **/
-int		ssigns(float in);
+float		ssigns(float in);
 
 /**
  ** \brief Double Signe function
- ** Determine the sine of in (assume that 0 is positive).
+ ** Determine the sign of in (assume that 0 is positive).
  ** \param in : the double we must determine sign.
  ** \return -1 or +1 depending on the sign of in.
  **/
-int		dsigns(double in);
+double		dsigns(double in);
 
 /**
  ** \brief Float Complex Signe function
- ** Determine the sine of in (assume that 0 is positive).
+ ** Determine the sign of in (assume that 0 is positive).
  ** \param in : the float we must determine sign.
  ** \return -1 or +1 depending on the sign of in.
  **/
@@ -44,7 +44,7 @@ floatComplex	csigns(floatComplex in);
 
 /**
  ** \brief Double Complex Signe function
- ** Determine the sine of in (assume that 0 is positive).
+ ** Determine the sign of in (assume that 0 is positive).
  ** \param in : the double we must determine sign.
  ** \return -1 or +1 depending on the sign of in.
  **/
@@ -52,7 +52,7 @@ doubleComplex	zsigns(doubleComplex in);
 
 /**
  ** \brief Float Signe Array function
- ** Determine the sine of an array in (assume that 0 is positive).
+ ** Determine the sign of an array in (assume that 0 is positive).
  ** \param in : the float array we must determine sign.
  ** \param size : the number of elements.
  ** \return -1 or +1 depending on the sign of in elements.
@@ -61,7 +61,7 @@ void	ssigna(float *in, int size, float *out);
 
 /**
  ** \brief Double Signe Array function
- ** Determine the sine of an array in (assume that 0 is positive).
+ ** Determine the sign of an array in (assume that 0 is positive).
  ** \param in : the double array we must determine sign.
  ** \param size : the number of elements.
  ** \return -1 or +1 depending on the sign of in elements.
@@ -70,7 +70,7 @@ void	dsigna(double *in, int size, double *out);
 
 /**
  ** \brief Float Signe Complex Array function
- ** Determine the sine of an array in (assume that 0 is positive).
+ ** Determine the sign of an array in (assume that 0 is positive).
  ** \param in : the float complex array we must determine sign.
  ** \param size : the number of elements.
  ** \return -1 or +1 depending on the sign of in elements.
@@ -79,7 +79,7 @@ void	csigna(floatComplex *in, int size, floatComplex *out);
 
 /**
  ** \brief Double Signe Complex Array function
- ** Determine the sine of an array in (assume that 0 is positive).
+ ** Determine the sign of an array in (assume that 0 is positive).
  ** \param in : the double complex array we must determine sign.
  ** \param size : the number of elements.
  ** \return -1 or +1 depending on the sign of in elements.
