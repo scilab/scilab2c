06da3df2ff648931ce65ed91bf516062828b2354
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2006-12-01 15:37:45 +0000

    Quite a good DouComplex lib... Will probably be improved later...

--- src/type/doubleComplex.h
+++ src/type/doubleComplex.h
@@ -5,13 +5,15 @@
 ** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
 **
 ** Started on  Thu Nov 30 16:50:08 2006 jofret
-** Last update Thu Nov 30 17:53:13 2006 jofret
+** Last update Fri Dec  1 15:17:05 2006 jofret
 **
 ** Copyright INRIA 2006
 */
 
 #ifndef __DOUBLECOMPLEX_H__
 #define __DOUBLECOMPLEX_H__
+
+#include <stdbool.h>
 
 struct  double_complex
 {
@@ -24,5 +26,7 @@ typedef struct double_complex doubleComplex;
 double		real(doubleComplex);
 double		imag(doubleComplex);
 doubleComplex	DoubleComplex(double, double);
+bool		isreal(doubleComplex);
+bool		isimag(doubleComplex);
 
 #endif /* !__DOUBLECOMPLEX_H__ */
