c221c8119a10e4d09cd103d07639c27536c3ae4d
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2006-12-07 17:03:31 +0000

    Begin basic library Cos

---
+++ scilab2c/src/elementaryFunctions/cos/dcoss.c
@@ -0,0 +1,17 @@
+/*
+**  -*- C -*-
+**
+** dcoss.c
+** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
+**
+** Started on  Thu Dec  7 12:02:41 2006 jofret
+** Last update Thu Dec  7 17:33:14 2006 jofret
+**
+** Copyright INRIA 2006
+*/
+
+#include <math.h>
+
+double	       dcoss(double x) {
+  return (cos(x));
+}
---
+++ scilab2c/src/elementaryFunctions/cos/scoss.c
@@ -0,0 +1,17 @@
+/*
+**  -*- C -*-
+**
+** scoss.c
+** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
+**
+** Started on  Thu Dec  7 11:05:37 2006 jofret
+** Last update Thu Dec  7 17:32:52 2006 jofret
+**
+** Copyright INRIA 2006
+*/
+
+#include <math.h>
+
+float		scoss(float x) {
+  return (cos(x));
+}
