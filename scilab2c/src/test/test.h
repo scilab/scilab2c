31569bb7501d34fedda45fb1a588568dc511ab86
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2006-12-08 16:03:57 +0000

    testing tool...

---
+++ scilab2c/src/test/test.h
@@ -0,0 +1,22 @@
+/*
+**  -*- C -*-
+**
+** test.h
+** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
+**
+** Started on  Fri Dec  8 15:00:40 2006 jofret
+** Last update Fri Dec  8 15:04:17 2006 jofret
+**
+** Copyright INRIA 2006
+*/
+
+/*
+** \brief Cosine Test
+*/
+int testCos();
+
+/*
+** \brief Sine Test
+*/
+int testSin();
+
