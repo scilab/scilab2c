5e9281a833792577ba46e95e75fb3efcf9e8e651
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2006-12-08 16:03:14 +0000

    Make some adjustements

--- src/elementaryFunctions/cos/ccoss.c
+++ src/elementaryFunctions/cos/ccoss.c
@@ -5,7 +5,7 @@
 ** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
 **
 ** Started on  Thu Dec  7 12:04:28 2006 jofret
-** Last update Thu Dec  7 15:25:09 2006 jofret
+** Last update Fri Dec  8 12:05:56 2006 jofret
 **
 ** Copyright INRIA 2006
 */
@@ -16,7 +16,6 @@
 #include <complex.h>
 typedef float complex floatComplex;
 #endif
-
 
 floatComplex		ccoss(floatComplex z) {
   /* FIXME: Dummy... */
