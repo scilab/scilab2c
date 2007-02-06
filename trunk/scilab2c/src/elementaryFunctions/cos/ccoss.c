c146a3d40e8f4e80c69efd8d6e27efa66c01011d
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2007-02-06 10:59:29 +0000

    * src/type : 	Add C99 compatibility. Float implementation. 	* src/elementaryFunctions/cos/zcoss.c 	Fixed with C99 Complex but Scilab computation. 	* src/elementaryFunctions/cos/ccoss.c 	Fixed with C99 Complex but Scilab computation.

--- trunk/scilab2c/src/elementaryFunctions/cos/ccoss.c
+++ trunk/scilab2c/src/elementaryFunctions/cos/ccoss.c
@@ -5,14 +5,22 @@
 ** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
 **
 ** Started on  Thu Dec  7 12:04:28 2006 jofret
-** Last update Wed Jan 31 10:19:53 2007 jofret
+** Last update Mon Feb  5 17:24:00 2007 jofret
 **
 ** Copyright INRIA 2006
 */
 
 #include "floatComplex.h"
 
+float	scoss(float);
+float	scoshs(float);
+float	ssins(float);
+float	ssinhs(float);
+
 floatComplex		ccoss(floatComplex z) {
-  /* FIXME: Dummy... */
-  return z;
+  float real = creal(z);
+  float imag = cimag(z);
+
+  return FloatComplex(scoss(real) * scoshs(imag),
+		      -ssins(real) * ssinhs(imag));
 }
