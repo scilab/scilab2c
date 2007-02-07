6d8063dca2d89d72e359eb76d42cbde87acb32b7
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2007-02-07 07:36:06 +0000

    * src/elementaryFunctions/cosh/zcoshs.c Fixed with C99 Complex but Scilab computation. * src/elementaryFunctions/cosh/ccoshs.c Fixed with C99 Complex but Scilab computation. * src/elementaryFunctions/cosh/zsins.c Fixed with C99 Complex but Scilab computation. * src/elementaryFunctions/cosh/csins.c Fixed with C99 Complex but Scilab computation.

--- scilab2c/src/elementaryFunctions/sin/csins.c
+++ scilab2c/src/elementaryFunctions/sin/csins.c
@@ -5,14 +5,21 @@
 ** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
 **
 ** Started on  Fri Dec  8 12:04:39 2006 jofret
-** Last update Wed Jan 31 11:13:02 2007 jofret
+** Last update Tue Feb  6 15:56:55 2007 jofret
 **
 ** Copyright INRIA 2006
 */
 
 #include "floatComplex.h"
 
+float	ssins(float);
+float	scoss(float);
+float	ssinhs(float);
+float	scoshs(float);
+
 floatComplex csins(floatComplex z) {
-  /* FIXME: Dummy... */
-  return z;
+  float real = creal(z);
+  float imag = cimag(z);
+
+  return(FloatComplex(ssins(real) * scoshs(imag), scoss(real) * ssinhs(imag)));
 }
