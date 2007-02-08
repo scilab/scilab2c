d35e734016e6b765708498ea10f386063537f8e3
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2007-02-08 12:36:46 +0000

    * src/auxiliaryFunctions/sign : Signe of a float or double. * src/auxiliaryFunctions/sign/sign.h : Interface * src/auxiliaryFunctions/sign/{sd}signs.c : Implementation : s(float) d(double)
    Sign module and lib

---
+++ src/auxiliaryFunctions/sign/sign.h
@@ -0,0 +1,28 @@
+/*
+**  -*- C -*-
+**
+** sign.h
+** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
+**
+** Started on  Thu Feb  8 10:12:17 2007 jofret
+** Last update Thu Feb  8 10:31:00 2007 jofret
+**
+** Copyright INRIA 2007
+*/
+
+#ifndef __SIGN_H__
+#define __SIGN_H__
+
+#include <math.h>
+
+/*
+** \brief Float Signe function
+*/
+int	ssigns(float);
+
+/*
+** \brief Double Signe function
+*/
+int	dsigns(double);
+
+#endif /* !__SIGN_H__ */
