3dc33c3ebf0f73c9ecb2db2a012449501c687bfe
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2007-02-08 15:02:12 +0000

    Modify some unexpected calls...

--- scilab2c/src/auxiliaryFunctions/sign/ssigns.c
+++ scilab2c/src/auxiliaryFunctions/sign/ssigns.c
@@ -5,7 +5,7 @@
 ** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
 **
 ** Started on  Thu Feb  8 10:26:22 2007 jofret
-** Last update Thu Feb  8 11:41:35 2007 jofret
+** Last update Thu Feb  8 15:58:54 2007 jofret
 **
 ** Copyright INRIA 2007
 */
@@ -13,7 +13,7 @@
 #include "sign.h"
 
 int ssigns(float num) {
-  if (num > 0) {
+  if (num >= 0) {
     return 1;
   }
   return -1;
