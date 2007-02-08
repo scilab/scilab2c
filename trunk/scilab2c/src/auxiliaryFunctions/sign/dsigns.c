2328694017711c195f2a203c37f5efc064c6002c
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2007-02-08 15:02:12 +0000

    Modify some unexpected calls...

--- trunk/scilab2c/src/auxiliaryFunctions/sign/dsigns.c
+++ trunk/scilab2c/src/auxiliaryFunctions/sign/dsigns.c
@@ -5,7 +5,7 @@
 ** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
 **
 ** Started on  Thu Feb  8 10:26:22 2007 jofret
-** Last update Thu Feb  8 11:41:58 2007 jofret
+** Last update Thu Feb  8 15:58:44 2007 jofret
 **
 ** Copyright INRIA 2007
 */
@@ -13,7 +13,7 @@
 #include "sign.h"
 
 int dsigns(double num) {
-  if (num > 0) {
+  if (num >= 0) {
     return 1;
   }
   return -1;
