1bd369abbf249acf1602eb74fb5ab88b5952c271
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2007-02-08 12:36:46 +0000

    * src/auxiliaryFunctions/sign : Signe of a float or double. * src/auxiliaryFunctions/sign/sign.h : Interface * src/auxiliaryFunctions/sign/{sd}signs.c : Implementation : s(float) d(double)
    Sign module and lib

---
+++ trunk/scilab2c/src/auxiliaryFunctions/sign/sign.c
@@ -0,0 +1,22 @@
+/*
+**  -*- C -*-
+**
+** sign.c
+** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
+**
+** Started on  Wed Feb  7 17:13:43 2007 jofret
+** Last update Wed Feb  7 17:15:48 2007 jofret
+**
+** Copyright INRIA 2007
+*/
+
+
+#include "misc.h"
+
+int sign(float f) {
+
+}
+
+int sign(double f) {
+
+}
---
+++ trunk/scilab2c/src/auxiliaryFunctions/sign/ssigns.c
@@ -0,0 +1,20 @@
+/*
+**  -*- C -*-
+**
+** ssigns.c
+** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
+**
+** Started on  Thu Feb  8 10:26:22 2007 jofret
+** Last update Thu Feb  8 11:41:35 2007 jofret
+**
+** Copyright INRIA 2007
+*/
+
+#include "sign.h"
+
+int ssigns(float num) {
+  if (num > 0) {
+    return 1;
+  }
+  return -1;
+}
