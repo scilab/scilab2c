7a2ddb83da35f20a8b9f9af26f6e94851b4e5dbb
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2007-02-09 16:13:04 +0000

    * src/elementaryFunctions/cexps.c : Implementation. * src/elementaryFunctions/zexps.c : Implementation.

--- trunk/scilab2c/src/elementaryFunctions/exp/cexps.c
+++ trunk/scilab2c/src/elementaryFunctions/exp/cexps.c
@@ -5,14 +5,21 @@
 ** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
 **
 ** Started on  Thu Dec  7 12:04:28 2006 jofret
-** Last update Wed Jan 31 11:13:43 2007 jofret
+** Last update Fri Feb  9 17:00:38 2007 jofret
 **
 ** Copyright INRIA 2006
 */
 
 #include "floatComplex.h"
 
+float	sexps(float);
+float	scoss(float);
+float	ssins(float);
+
 floatComplex		cexps(floatComplex z) {
-  /* FIXME: Dummy... */
-  return z;
+  float real = creal(z);
+  float imag = cimag(z);
+
+  return FloatComplex(sexps(real)*scoss(imag),
+		      sexps(real)*ssins(imag));
 }
