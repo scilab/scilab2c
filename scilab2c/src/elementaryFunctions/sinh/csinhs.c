d0723bccc548f70afe8f544464a9b5fb057b82d4
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2007-02-12 15:32:48 +0000

    * src/elementaryFunctions/sinh/zsinhs.c Fixed with C99 Complex but Scilab computation. * src/elementaryFunctions/sinh/csinhs.c Fixed with C99 Complex but Scilab computation.
    Fix some mistakes

--- scilab2c/src/elementaryFunctions/sinh/csinhs.c
+++ scilab2c/src/elementaryFunctions/sinh/csinhs.c
@@ -5,14 +5,19 @@
 ** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
 **
 ** Started on  Fri Dec  8 12:04:39 2006 jofret
-** Last update Wed Jan 31 11:12:44 2007 jofret
+** Last update Mon Feb 12 16:15:52 2007 jofret
 **
 ** Copyright INRIA 2006
 */
 
 #include "floatComplex.h"
 
+floatComplex	csins(floatComplex);
+
 floatComplex csinhs(floatComplex z) {
-  /* FIXME: Dummy... */
-  return z;
+  float real = creal(z);
+  float imag = cimag(z);
+
+  floatComplex result = csins(FloatComplex(-imag, real));
+  return (FloatComplex(cimag(result), -creal(result)));
 }
