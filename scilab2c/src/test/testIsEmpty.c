9a58283ed04baed499134ff949152303d13034a5
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2007-02-14 16:58:30 +0000

    Adding missing tests files.
    Must have been linked earlier with the "Sign" lib.
    Correct the mistake.

---
+++ scilab2c/src/test/testIsEmpty.c
@@ -0,0 +1,19 @@
+/*
+**  -*- C -*-
+**
+** testIsEmpty.c
+** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
+**
+** Started on  Wed Feb 14 16:07:57 2007 jofret
+** Last update Wed Feb 14 17:31:54 2007 jofret
+**
+** Copyright INRIA 2007
+*/
+
+#include <stdio.h>
+
+int testIsEmpty() {
+  printf("\n>>>> IsEmpty Tests\n");
+  /* FIXME : Implement some test here ... */
+  return 0;
+}
