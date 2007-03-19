1f7a61cb8e9162adc2147066e11e25ccf6903cef
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2007-03-19 13:46:00 +0000

    Correction of functions profile :
    Remove "stride" parameters.
    
    Add dioxygen style documentation.

--- trunk/scilab2c/src/elementaryFunctions/includes/sinh.h
+++ trunk/scilab2c/src/elementaryFunctions/includes/sinh.h
@@ -5,7 +5,7 @@
 ** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
 **
 ** Started on  Tue Dec  5 15:49:18 2006 jofret
-** Last update Fri Feb 23 17:18:37 2007 jofret
+** Last update Mon Mar 19 13:46:39 2007 jofret
 **
 ** Copyright INRIA 2006
 */
@@ -19,42 +19,58 @@
 
 /*
 ** \brief Float Sine function
+** \param in : input value.
 */
-float		ssinhs(float);
+float		ssinhs(float in);
 
 /*
 ** \brief Double Sine function
+** \param in : input value.
 */
-double		dsinhs(double);
+double		dsinhs(double in);
 
 /*
 ** \brief Float Complex Sine function
+** \param in : input value.
 */
-floatComplex	csinhs(floatComplex);
+floatComplex	csinhs(floatComplex in);
 
 /*
 ** \brief Double Complex Sine function
+** \param in : input value.
 */
-doubleComplex	zsinhs(doubleComplex);
+doubleComplex	zsinhs(doubleComplex in);
 
 /*
 ** \brief Float Matrix Sine function
+** \param in : input array value.
+** \param out : input array value.
+** \param size : the size of in and out arrays.
 */
-void		ssinha(float*, int, float*, int, int);
+void		ssinha(float* in, float* out, int size);
 
 /*
 ** \brief Double Matrix Sine function
+** \param in : input array value.
+** \param out : input array value.
+** \param size : the size of in and out arrays.
 */
-void		dsinha(double*, int, double*, int, int);
+void		dsinha(double* in, double* out, int size);
 
 /*
 ** \brief Float Complex Matrix Sine function
+** \param in : input array value.
+** \param out : input array value.
+** \param size : the size of in and out arrays.
 */
-void		csinha(floatComplex*, int, floatComplex*, int, int);
+void		csinha(floatComplex* in, floatComplex* out, int size);
 
 /*
 ** \brief Double Complex Matrix Sine function
+** \param in : input array value.
+** \param out : input array value.
+** \param size : the size of in and out arrays.
 */
-void		zsinha(doubleComplex*, int, doubleComplex*, int, int);
+void		zsinha(doubleComplex* in, doubleComplex* out, int size);
 
 
