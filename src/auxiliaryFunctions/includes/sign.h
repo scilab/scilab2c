525166f17511bd69e305f7af2e54b17c35e70f19
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2007-03-20 14:19:38 +0000

    Doxygen documentation

--- src/auxiliaryFunctions/includes/sign.h
+++ src/auxiliaryFunctions/includes/sign.h
@@ -5,7 +5,7 @@
 ** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
 **
 ** Started on  Thu Feb  8 10:12:17 2007 jofret
-** Last update Thu Feb  8 10:31:00 2007 jofret
+** Last update Tue Mar 20 14:58:52 2007 jofret
 **
 ** Copyright INRIA 2007
 */
@@ -15,14 +15,20 @@
 
 #include <math.h>
 
-/*
-** \brief Float Signe function
-*/
-int	ssigns(float);
+/**
+ ** \brief Float Signe function
+ ** Determine the sine of in (assume that 0 is positive).
+ ** \param in : the float we must determine sign.
+ ** \return -1 or +1 depending on the sign of in.
+ **/
+int	ssigns(float in);
 
-/*
-** \brief Double Signe function
-*/
-int	dsigns(double);
+/**
+ ** \brief Double Signe function
+ ** Determine the sine of in (assume that 0 is positive).
+ ** \param in : the double we must determine sign.
+ ** \return -1 or +1 depending on the sign of in.
+ **/
+int	dsigns(double in);
 
 #endif /* !__SIGN_H__ */
