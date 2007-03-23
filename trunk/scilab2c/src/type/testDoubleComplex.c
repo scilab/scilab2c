8648de55bdaf4855b4f43eb160e44bc77e432ca0
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2007-03-23 10:05:02 +0000

    * src/type/Makefile : Removed : delegate to AutoTools. * src/type/Makefile.am : Automake file. * src/type/Makefile.in : Automake generated file : ./configure needed. * src/type/testFloatComplex.c : Test Float Complex generation : Assert + AutoCheck through AutoTools. * src/type/testDoubleComplex.c : Test Double Complex generation : Assert + AutoCheck through AutoTools.

--- trunk/scilab2c/src/type/testDoubleComplex.c
+++ trunk/scilab2c/src/type/testDoubleComplex.c
@@ -5,36 +5,30 @@
 ** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
 **
 ** Started on  Thu Nov 30 16:59:04 2006 jofret
-** Last update Fri Feb 23 16:19:35 2007 jofret
+** Last update Fri Mar 23 11:11:25 2007 jofret
 **
 ** Copyright INRIA 2006
 */
 
-#ifdef __STDC_VERSION__
-#  ifndef STDC
-#    define STDC
-#  endif
-#  if __STDC_VERSION__ >= 199901L
-#    ifndef STDC99
-#      define STDC99
-#    endif
-#  endif
-#endif
-
-#ifndef STDC99
-#  include <stdio.h>
-#  include "doubleComplex.h"
-#else
-#  include <stdio.h>
-#  include <complex.h>
-#  include "doubleComplexC99.h"
-#endif
-
+#include <stdio.h>
+#include <assert.h>
+#include "doubleComplex.h"
 
 int main(int argc, char **argv) {
   /* z = -3 + 25*%i */
   doubleComplex z = DoubleComplex(-3,25);
   printf("Partie reelle = %f\n", zreals(z));
+  assert(zreals(z) == -3);
   printf("Partie imaginaire = %f\n", zimags(z));
+  assert(zimags(z) == 25);
+
+  /* z = -3.123456 + 25.123456*%i */
+  z = DoubleComplex(-3.123456,25.123456);
+  printf("Partie reelle = %f\n", zreals(z));
+  assert(zreals(z) == (double)-3.123456);
+  printf("Partie imaginaire = %f\n", zimags(z));
+  assert(zimags(z) == (double)25.123456);
+
+
   return 0;
 }
