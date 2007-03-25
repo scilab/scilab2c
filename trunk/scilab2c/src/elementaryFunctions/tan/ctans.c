2714a1bb65e153bbc23b51812d4509f8cbeba786
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2007-03-25 19:51:02 +0000

    Autotools

--- trunk/scilab2c/src/elementaryFunctions/tan/ctans.c
+++ trunk/scilab2c/src/elementaryFunctions/tan/ctans.c
@@ -5,21 +5,27 @@
 ** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
 **
 ** Started on  Thu Dec  7 12:04:28 2006 jofret
-** Last update Wed Mar 21 16:23:08 2007 jofret
+** Last update Sun Mar 25 12:23:06 2007 bruno
 **
 ** Copyright INRIA 2006
 */
 
 #include "tan.h"
-#include "log.h"
+/*
+ #include "log.h"
+*/
 #include "sqrt.h"
-#include "lapack.h"
+/*
+ #include "lapack.h"
+*/
 
 floatComplex		ctans(floatComplex z) {
 
   double out_real = 0;
   double out_imag = 0;
-  double L = 1 + dlogs(2 / dsqrts(dlamch('e', 1)));
+  /*
+    double L = 1 + dlogs(2 / dsqrts(dlamch('e', 1)));
+  */
 
   return FloatComplex(out_real, out_imag);
 }
