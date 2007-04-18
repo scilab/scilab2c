41897e89802474bdc9fba4ecb8910a81f23f4c8e
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2007-04-18 16:56:05 +0000

    Moving constants management

---
+++ trunk/scilab2c/src/includes/constant.h
@@ -0,0 +1,19 @@
+/*
+**  -*- C -*-
+**
+** constant.h
+** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
+**
+** Started on  Fri Mar 30 12:03:14 2007 jofret
+** Last update Fri Mar 30 12:03:48 2007 jofret
+**
+** Copyright INRIA 2007
+*/
+
+#ifndef _CONSTANT_H_
+#define _CONSTANT_H_
+
+#define PI 3.1415826535
+
+#endif /* !_CONSTANT_H_ */
+
