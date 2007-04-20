8e7ea9e904a0e9c5760d2b05de5b3f0af31785de
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2007-04-20 08:29:01 +0000

    Adding automated tests.

---
+++ src/elementaryFunctions/acos/testAcos.h
@@ -0,0 +1,40 @@
+/*
+**  -*- C -*-
+**
+** testAcos.h
+** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
+**
+** Started on  Mon Mar 26 16:33:11 2007 jofret
+** Last update Fri Apr 20 10:33:19 2007 jofret
+**
+** Copyright INRIA 2007
+*/
+
+#ifndef _TESTACOS_H_
+#define _TESTACOS_H_
+
+#include <stdio.h>
+#include <assert.h>
+#include "acos.h"
+#include "constant.h"
+
+
+void sacossTest(void);
+
+void dacossTest(void);
+
+void cacossTest(void);
+
+void zacossTest(void);
+
+void sacosaTest(void);
+
+void dacosaTest(void);
+
+void cacosaTest(void);
+
+void zacosaTest(void);
+
+int testAcos(void);
+
+#endif /* ! _TESTACOS_H_ */
