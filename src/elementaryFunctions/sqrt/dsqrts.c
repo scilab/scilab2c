253684509ac9a972c5211feb256b8d7c5f8b2051
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2007-04-20 08:47:10 +0000

    Square Root begin implementation.

---
+++ src/elementaryFunctions/sqrt/dsqrts.c
@@ -0,0 +1,18 @@
+/*
+**  -*- C -*-
+**
+** dsqrts.c
+** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
+**
+** Started on  Fri Apr 20 10:59:31 2007 jofret
+** Last update Fri Apr 20 11:00:09 2007 jofret
+**
+** Copyright INRIA 2007
+*/
+
+#include "sqrt.h"
+
+double		dsqrts(double in){
+  /* FIXME : Implementation */
+  return 0;
+}
---
+++ src/elementaryFunctions/sqrt/ssqrts.c
@@ -0,0 +1,18 @@
+/*
+**  -*- C -*-
+**
+** ssqrts.c
+** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
+**
+** Started on  Fri Apr 20 10:57:05 2007 jofret
+** Last update Fri Apr 20 10:58:54 2007 jofret
+**
+** Copyright INRIA 2007
+*/
+
+#include "sqrt.h"
+
+float		ssqrts(float in) {
+  /* FIXME : Implementation */
+  return 0;
+}
