e858b16fbefe7735d94a9ec62706739021f4f07e
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2007-04-20 09:14:27 +0000

    Ending Sine implementation and tests.

--- trunk/scilab2c/src/elementaryFunctions/sin/testSin.c
+++ trunk/scilab2c/src/elementaryFunctions/sin/testSin.c
@@ -5,7 +5,7 @@
 ** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
 **
 ** Started on  Fri Dec  8 15:06:16 2006 jofret
-** Last update Fri Mar 30 11:12:32 2007 jofret
+** Last update Fri Apr 20 11:25:31 2007 jofret
 **
 ** Copyright INRIA 2006
 */
@@ -42,19 +42,105 @@ void dsinsTest() {
   printf("dsins(-PI/6) = %e\n", dsins(-PI/6));
 }
 
+void csinsTest(void) {
+  floatComplex pi_pi = FloatComplex(PI, PI);
+  floatComplex pi_2_pi_2 = FloatComplex(PI/2, PI/2);
+  floatComplex pi_2_pi_3 = FloatComplex(PI/2, PI/3);
+  floatComplex pi_2_pi_4 = FloatComplex(PI/2, PI/4);
+  floatComplex out;
+
+  printf(">> Float Complex scalar\n");
+  out = csins(pi_pi);
+  printf("csins(PI + I*PI) = %f + I * %f\n", creals(out), cimags(out));
+  out = csins(pi_2_pi_2);
+  printf("csins(PI/2 + I*PI/2) = %f + I * %f\n", creals(out), cimags(out));
+  out = csins(pi_2_pi_3);
+  printf("csins(PI/2 + I*PI/3) = %f + I * %f\n", creals(out), cimags(out));
+  out = csins(pi_2_pi_4);
+  printf("csins(PI/2 + I*PI/4) = %f + I * %f\n", creals(out), cimags(out));
+}
+
+void zsinsTest(void) {
+  doubleComplex pi_pi = DoubleComplex(PI, PI);
+  doubleComplex pi_2_pi_2 = DoubleComplex(PI/2, PI/2);
+  doubleComplex pi_2_pi_3 = DoubleComplex(PI/2, PI/3);
+  doubleComplex pi_2_pi_4 = DoubleComplex(PI/2, PI/4);
+  doubleComplex out;
+
+  printf(">> Double Complex scalar\n");
+  out = zsins(pi_pi);
+  printf("zsins(PI + I*PI) = %e + I * %e\n", zreals(out), zimags(out));
+  out = zsins(pi_2_pi_2);
+  printf("zsins(PI/2 + I*PI/2) = %e + I * %e\n", zreals(out), zimags(out));
+  out = zsins(pi_2_pi_3);
+  printf("zsins(PI/2 + I*PI/3) = %e + I * %e\n", zreals(out), zimags(out));
+  out = zsins(pi_2_pi_4);
+  printf("zsins(PI/2 + I*PI/4) = %e + I * %e\n", zreals(out), zimags(out));
+}
+
+void ssinaTest(void) {
+  float out[5];
+  float in[5] = {PI, PI/2, PI/3, PI/4, PI/6};
+  int i = 0;
+
+  printf(">> Float array\n");
+  ssina(in, out, 5);
+  for (i = 0 ; i < 5 ; ++i)
+    printf("ssina(array) = %f\n", out[i]);
+}
+
+void dsinaTest(void) {
+  double out[5];
+  double in[5] = {PI, PI/2, PI/3, PI/4, PI/6};
+  int i = 0;
+
+  printf(">> Double Array\n");
+  dsina(in, out, 5);
+  for (i = 0 ; i < 5 ; ++i)
+    printf("ssina(array) = %f\n", out[i]);
+
+}
+
+void csinaTest(void) {
+  floatComplex pi_pi = FloatComplex(PI, PI);
+  floatComplex pi_2_pi_2 = FloatComplex(PI/2, PI/2);
+  floatComplex pi_2_pi_3 = FloatComplex(PI/2, PI/3);
+  floatComplex pi_2_pi_4 = FloatComplex(PI/2, PI/4);
+  floatComplex in[4] = {pi_pi, pi_2_pi_2, pi_2_pi_3, pi_2_pi_4};
+  floatComplex out[4];
+  int i = 0;
+
+  csina(in, out, 4);
+  printf(">> Float Complex Array\n");
+  for (i = 0 ; i < 4 ; ++i)
+    printf("csina(array) = %e + I * %e\n", creals(out[i]), cimags(out[i]));
+}
+
+void zsinaTest(void) {
+  doubleComplex pi_pi = DoubleComplex(PI, PI);
+  doubleComplex pi_2_pi_2 = DoubleComplex(PI/2, PI/2);
+  doubleComplex pi_2_pi_3 = DoubleComplex(PI/2, PI/3);
+  doubleComplex pi_2_pi_4 = DoubleComplex(PI/2, PI/4);
+  doubleComplex in[4] = {pi_pi, pi_2_pi_2, pi_2_pi_3, pi_2_pi_4 };
+  doubleComplex out[4];
+  int i = 0;
+
+  zsina(in, out, 4);
+  printf(">> Double Complex Array\n");
+  for (i = 0 ; i < 4 ; ++i)
+    printf("zsina(array) = %e + I * %e\n", zreals(out[i]), zimags(out[i]));
+}
+
 int testSin() {
   printf("\n>>>> Sine Tests\n");
   ssinsTest();
   dsinsTest();
-  /* FIXME : Implement some test here ... */
-  /*
-    csinsTest();
-    zsinsTest();
-    ssinaTest();
-    dsinaTest();
-    csinaTest();
-    zsinaTest();
-  */
+  csinsTest();
+  zsinsTest();
+  ssinaTest();
+  dsinaTest();
+  csinaTest();
+  zsinaTest();
   return 0;
 }
 
