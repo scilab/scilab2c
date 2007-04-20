81dc9f333be2bfffb8744151c51f635a7439e673
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2007-04-20 08:46:13 +0000

    Adding Automated tests.

---
+++ trunk/scilab2c/src/elementaryFunctions/sinh/testSinh.h
@@ -0,0 +1,27 @@
+/*
+**  -*- C -*-
+**
+** testSinh.h
+** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
+**
+** Started on  Fri Mar 30 12:01:28 2007 jofret
+** Last update Fri Mar 30 12:08:11 2007 jofret
+**
+** Copyright INRIA 2007
+*/
+
+#ifndef _TESTSINH_H_
+#define _TESTSINH_H_
+
+#include <stdio.h>
+#include <assert.h>
+#include "sinh.h"
+#include "constant.h"
+
+void ssinhsTest(void);
+
+void dsinhsTest(void);
+
+int testSinh(void);
+
+#endif /* !_TESTSINH_H_ */
---
+++ trunk/scilab2c/src/elementaryFunctions/sqrt/testSqrt.h
@@ -0,0 +1,27 @@
+/*
+**  -*- C -*-
+**
+** testSqrt.h
+** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
+**
+** Started on  Fri Mar 30 12:01:28 2007 jofret
+** Last update Fri Apr 20 10:52:56 2007 jofret
+**
+** Copyright INRIA 2007
+*/
+
+#ifndef _TESTSQRT_H_
+#define _TESTSQRT_H_
+
+#include <stdio.h>
+#include <assert.h>
+#include "sqrt.h"
+#include "constant.h"
+
+void ssqrtsTest(void);
+
+void dsqrtsTest(void);
+
+int testSqrt(void);
+
+#endif /* !_TESTSQRT_H_ */
