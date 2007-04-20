43b4c8146e3e9b67a6c36405ff4b7192b567f50c
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2007-04-20 12:34:21 +0000

    Log Implementation used for tangeant.

---
+++ trunk/scilab2c/src/elementaryFunctions/log/slogs.c
@@ -0,0 +1,18 @@
+/*
+**  -*- C -*-
+**
+** slogs.c
+** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
+**
+** Started on  Fri Apr 20 14:25:04 2007 jofret
+** Last update Fri Apr 20 14:46:34 2007 jofret
+**
+** Copyright INRIA 2007
+*/
+
+#include <math.h>
+#include "log.h"
+
+float		slogs(float in) {
+  return (log(in));
+}
---
+++ trunk/scilab2c/src/elementaryFunctions/log/zlogs.c
@@ -0,0 +1,18 @@
+/*
+**  -*- C -*-
+**
+** zlogs.c
+** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
+**
+** Started on  Fri Apr 20 14:31:56 2007 jofret
+** Last update Fri Apr 20 14:34:52 2007 jofret
+**
+** Copyright INRIA 2007
+*/
+
+#include "log.h"
+
+doubleComplex	zlogs(doubleComplex in) {
+  /* FIXME : Implementation */
+  return DoubleComplex(0, 0);
+}
