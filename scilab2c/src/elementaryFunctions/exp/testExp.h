ff432a13e98fed9801ce14852f39c0eef3748ade
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2007-04-20 08:32:26 +0000

    Adding Automated tests.

---
+++ scilab2c/src/elementaryFunctions/exp/testExp.h
@@ -0,0 +1,40 @@
+/*
+**  -*- C -*-
+**
+** testExp.h
+** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
+**
+** Started on  Wed Apr 18 17:07:03 2007 jofret
+** Last update Wed Apr 18 17:07:43 2007 jofret
+**
+** Copyright INRIA 2007
+*/
+
+#ifndef _TESTEXP_H_
+#define _TESTEXP_H_
+
+#include <stdio.h>
+#include <assert.h>
+#include "exp.h"
+#include "constant.h"
+
+
+void sexpsTest(void);
+
+void dexpsTest(void);
+
+void cexpsTest(void);
+
+void zexpsTest(void);
+
+void sexpaTest(void);
+
+void dexpaTest(void);
+
+void cexpaTest(void);
+
+void zexpaTest(void);
+
+int testExp(void);
+
+#endif /* ! _TESTEXP_H_ */
