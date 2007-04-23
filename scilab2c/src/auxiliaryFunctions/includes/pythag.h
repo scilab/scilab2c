cb0b24c1961e1f83818bf788b2896da9eba34cd0
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2007-04-23 15:35:32 +0000

    Oups just forgot the header...

---
+++ scilab2c/src/auxiliaryFunctions/includes/pythag.h
@@ -0,0 +1,28 @@
+/*
+**  -*- C -*-
+**
+** pythag.h
+** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
+**
+** Started on  Thu Feb  8 10:12:17 2007 jofret
+** Last update Mon Apr 23 12:10:10 2007 jofret
+**
+** Copyright INRIA 2007
+*/
+
+#ifndef __PYTHAG_H__
+#define __PYTHAG_H__
+
+#include "sqrt.h"
+
+/*
+** \brief Float Pythag function
+*/
+float	spythags(float x, float y);
+
+/*
+** \brief Double Pythag function
+*/
+double	dpythags(double x, double y);
+
+#endif /* !__PYTHAG_H__ */
