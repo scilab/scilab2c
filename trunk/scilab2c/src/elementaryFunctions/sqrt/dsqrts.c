6d4ee4b1310f0f8aa4584d46ed7cad88fa8d2a35
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2007-04-24 16:14:25 +0000

    sqrt link to libMath

--- trunk/scilab2c/src/elementaryFunctions/sqrt/dsqrts.c
+++ trunk/scilab2c/src/elementaryFunctions/sqrt/dsqrts.c
@@ -5,14 +5,14 @@
 ** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
 **
 ** Started on  Fri Apr 20 10:59:31 2007 jofret
-** Last update Fri Apr 20 11:00:09 2007 jofret
+** Last update Mon Apr 23 17:37:02 2007 jofret
 **
 ** Copyright INRIA 2007
 */
 
+#include <math.h>
 #include "sqrt.h"
 
 double		dsqrts(double in){
-  /* FIXME : Implementation */
-  return 0;
+    return sqrt(in);
 }
