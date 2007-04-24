c961bead61078940b2ed9794861ee8fbbae0468f
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2007-04-24 16:14:25 +0000

    sqrt link to libMath

--- scilab2c/src/elementaryFunctions/sqrt/ssqrts.c
+++ scilab2c/src/elementaryFunctions/sqrt/ssqrts.c
@@ -5,14 +5,14 @@
 ** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
 **
 ** Started on  Fri Apr 20 10:57:05 2007 jofret
-** Last update Fri Apr 20 10:58:54 2007 jofret
+** Last update Mon Apr 23 17:36:16 2007 jofret
 **
 ** Copyright INRIA 2007
 */
 
+#include <math.h>
 #include "sqrt.h"
 
 float		ssqrts(float in) {
-  /* FIXME : Implementation */
-  return 0;
+  return sqrt(in);
 }
