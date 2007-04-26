e3015bb9e7815986386f95cf938258f17410a3aa
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2007-04-26 06:41:39 +0000

    [RELEASE] find function done.

--- scilab2c/src/auxiliaryFunctions/find/testFind.c
+++ scilab2c/src/auxiliaryFunctions/find/testFind.c
@@ -5,48 +5,103 @@
 ** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
 **
 ** Started on  Wed Feb 14 15:50:15 2007 jofret
-** Last update Fri Feb 23 18:09:45 2007 jofret
+** Last update Tue Apr 24 18:13:42 2007 jofret
 **
 ** Copyright INRIA 2007
 */
 
-#include <stdio.h>
-
-#include "find.h"
-
-#define ERROR 42
+#include "testFind.h"
 
 int sfindaTest() {
+  int result = 0;
+
   printf(">> Floats \n");
   float goodArray[5] = {0.,2.,3.,5.,10.};
   float badArray[5] = {0.,0.,0.,0.,0.};
-  if (sfinda(goodArray, 5) != 1) {
+  if (sfinda(goodArray, 5) == NOT_FOUND) {
+    printf("ERROR ! : Test Failed (non empty array)\n");
+    result = ERROR;
+  }
+  assert(sfinda(goodArray, 5) != NOT_FOUND);
+  assert(sfinda(goodArray, 5) == 1);
+  if (sfinda(badArray, 5) != NOT_FOUND) {
+    printf("ERROR ! : Test Failed (empty array)\n");
+    result = ERROR;
+  }
+  assert(sfinda(badArray, 5) == NOT_FOUND);
+  return result;
+}
+
+int dfindaTest() {
+  printf(">> Doubles \n");
+  double goodArray[5] = {0.,2.,3.,5.,10.};
+  double badArray[5] = {0.,0.,0.,0.,0.};
+  assert(dfinda(goodArray, 5) != NOT_FOUND);
+  assert(dfinda(goodArray, 5) == 1);
+  if (dfinda(goodArray, 5) == NOT_FOUND) {
     printf("ERROR ! : Test Failed (non empty array)\n");
     return ERROR;
   }
-  if (sfinda(badArray, 5) != NOT_FOUND) {
+  assert(dfinda(badArray, 5) == NOT_FOUND);
+  if (dfinda(badArray, 5) != NOT_FOUND) {
     printf("ERROR ! : Test Failed (empty array)\n");
     return ERROR;
   }
   return 0;
 }
 
-int dfindaTest() {
-  printf(">> Doubles \n");
-  /* FIXME : Implement some test here ... */
-  return 0;
-}
-
 int cfindaTest() {
+  int result = 0;
   printf(">> Float Complex \n");
-  /* FIXME : Implement some test here ... */
-  return 0;
+  floatComplex goodArray[5] = {FloatComplex(0., 0.),
+				FloatComplex(0., 2.),
+				FloatComplex(3., 50.),
+				FloatComplex(5., 10.),
+				FloatComplex(10., -10.)};
+  floatComplex badArray[5] = {FloatComplex(0., 0.),
+			       FloatComplex(0., 0.),
+			       FloatComplex(0., 0.),
+			       FloatComplex(0., 0.),
+			       FloatComplex(0., 0.)};
+  if (cfinda(goodArray, 5) == NOT_FOUND) {
+    printf("ERROR ! : Test Failed (non empty array)\n");
+    result = ERROR;
+  }
+  assert(cfinda(goodArray, 5) != NOT_FOUND);
+  assert(cfinda(goodArray, 5) == 1);
+  if (cfinda(badArray, 5) != NOT_FOUND) {
+    printf("ERROR ! : Test Failed (empty array)\n");
+    result = ERROR;
+  }
+  assert(cfinda(badArray, 5) == NOT_FOUND);
+  return result;
 }
 
 int zfindaTest() {
+ int result = 0;
   printf(">> Double Complex \n");
-  /* FIXME : Implement some test here ... */
-  return 0;
+ doubleComplex goodArray[5] = {DoubleComplex(0., 0.),
+				DoubleComplex(0., 2.),
+				DoubleComplex(3., 50.),
+				DoubleComplex(5., 10.),
+				DoubleComplex(10., -10.)};
+  doubleComplex badArray[5] = {DoubleComplex(0., 0.),
+			       DoubleComplex(0., 0.),
+			       DoubleComplex(0., 0.),
+			       DoubleComplex(0., 0.),
+			       DoubleComplex(0., 0.)};
+  if (zfinda(goodArray, 5) == NOT_FOUND) {
+    printf("ERROR ! : Test Failed (non empty array)\n");
+    result = ERROR;
+  }
+  assert(zfinda(goodArray, 5) != NOT_FOUND);
+  assert(zfinda(goodArray, 5) == 1);
+  if (zfinda(badArray, 5) != NOT_FOUND) {
+    printf("ERROR ! : Test Failed (empty array)\n");
+    result = ERROR;
+  }
+  assert(zfinda(badArray, 5) == NOT_FOUND);
+  return result;
 }
 
 int testFind() {
@@ -61,4 +116,9 @@ int testFind() {
 
   return (sfindaStatus + dfindaStatus +
 	  cfindaStatus + zfindaStatus);
+}
+
+int main(void) {
+  assert(testFind() == 0);
+  return 0;
 }
