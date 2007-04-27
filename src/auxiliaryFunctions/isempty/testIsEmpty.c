d4c521386b82ea3e29762a845bc4483f57bc056a
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2007-04-27 06:47:30 +0000

    [RELEASE] isEmpty functions ! (with simple low case name)

--- src/auxiliaryFunctions/isempty/testIsEmpty.c
+++ src/auxiliaryFunctions/isempty/testIsEmpty.c
@@ -5,63 +5,119 @@
 ** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
 **
 ** Started on  Wed Feb 14 16:07:57 2007 jofret
-** Last update Fri Feb 23 18:09:19 2007 jofret
+** Last update Fri Apr 27 09:00:55 2007 jofret
 **
 ** Copyright INRIA 2007
 */
 
-#include <stdio.h>
-
-#include "isEmpty.h"
-
-#define ERROR 51
+#include "testIsEmpty.h"
 
 int sisemptyaTest() {
+  int result = 0;
+
   printf(">> Float array\n");
+
   float empty[5] = {0., 0., 0., 0., 0.};
   float full[5] = {1., 2., 3., 0., 0.};
 
-  if (sisEmptya(empty, 5) == false) {
+  if (sisemptya(empty, 5) == false) {
     printf("ERROR ! : Test Failed (empty array)\n");
-    return ERROR;
+    result = ERROR;
   }
+  assert (sisemptya(empty, 5) == true);
 
-  if (sisEmptya(full, 5) == true) {
+  if (sisemptya(full, 5) == true) {
     printf("ERROR ! : Test Failed (non empty array)\n");
-    return ERROR;
+    result = ERROR;
   }
+  assert(sisemptya(full, 5) == false);
 
-  return 0;
+  return result;
 }
 
 int disemptyaTest() {
+  int result = 0;
+
   printf(">> Double array\n");
 
   double empty[5] = {0., 0., 0., 0., 0.};
   double full[5] = {1., 2., 3., 0., 0.};
 
-  if (disEmptya(empty, 5) == false) {
+  if (disemptya(empty, 5) == false) {
     printf("ERROR ! : Test Failed (empty array)\n");
-    return ERROR;
+    result = ERROR;
   }
+  assert(disemptya(empty, 5) == true);
 
-  if (disEmptya(full, 5) == true) {
+  if (disemptya(full, 5) == true) {
     printf("ERROR ! : Test Failed (non empty array)\n");
-    return ERROR;
+    result = ERROR;
   }
+  assert(disemptya(full, 5) == false);
 
-  return 0;
+  return result;
 }
 
 int cisemptyaTest() {
+  int result = 0;
   printf(">> Float Complex array\n");
 
-  return 0;
+  floatComplex empty[5] = {FloatComplex(0., 0.),
+			   FloatComplex(0., 0.),
+			   FloatComplex(0., 0.),
+			   FloatComplex(0., 0.),
+			   FloatComplex(0., 0.)};
+  floatComplex full[5] = {FloatComplex(0.,1.),
+			  FloatComplex(0., 2.),
+			  FloatComplex(0., 3.),
+			  FloatComplex(0., 0.),
+			  FloatComplex(0., 0.)};
+
+  if (cisemptya(empty, 5) == false) {
+    printf("ERROR ! : Test Failed (empty array)\n");
+    result = ERROR;
+  }
+  assert (cisemptya(empty, 5) == true);
+
+  if (cisemptya(full, 5) == true) {
+    printf("ERROR ! : Test Failed (non empty array)\n");
+    result = ERROR;
+  }
+  assert(cisemptya(full, 5) == false);
+
+
+  return result;
 }
 
 int zisemptyaTest() {
+  int result = 0;
+
   printf(">> Double Complex array\n");
-  return 0;
+  doubleComplex empty[5] = {DoubleComplex(0., 0.),
+			   DoubleComplex(0., 0.),
+			   DoubleComplex(0., 0.),
+			   DoubleComplex(0., 0.),
+			   DoubleComplex(0., 0.)};
+  doubleComplex full[5] = {DoubleComplex(0.,1.),
+			  DoubleComplex(0., 2.),
+			  DoubleComplex(0., 3.),
+			  DoubleComplex(0., 0.),
+			  DoubleComplex(0., 0.)};
+
+  if (zisemptya(empty, 5) == false) {
+    printf("ERROR ! : Test Failed (empty array)\n");
+    result = ERROR;
+  }
+  assert (zisemptya(empty, 5) == true);
+
+  if (zisemptya(full, 5) == true) {
+    printf("ERROR ! : Test Failed (non empty array)\n");
+    result = ERROR;
+  }
+  assert(zisemptya(full, 5) == false);
+
+
+  return result;
 }
 
 
@@ -76,4 +132,9 @@ int testIsEmpty() {
 
   return (sisemptyaTestStatus + disemptyaTestStatus +
 	  cisemptyaTestStatus + zisemptyaTestStatus);
+}
+
+int main(void) {
+  assert(testIsEmpty() == 0);
+  return 0;
 }
---
+++ src/auxiliaryFunctions/isempty/testIsEmpty.h
@@ -0,0 +1,32 @@
+/*
+**  -*- C -*-
+**
+** testIsEmpty.c
+** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
+**
+** Started on  Wed Feb 14 16:07:57 2007 jofret
+** Last update Fri Apr 27 08:59:49 2007 jofret
+**
+** Copyright INRIA 2007
+*/
+
+#ifndef __TESTISEMPTY_H__
+#define __TESTISEMPTY_H__
+
+#include <stdio.h>
+#include <assert.h>
+#include "isempty.h"
+
+#define ERROR 51
+
+int sisemptyaTest(void);
+
+int disemptyaTest(void);
+
+int cisemptyaTest(void);
+
+int zisemptyaTest(void);
+
+int testIsEmpty(void);
+
+#endif /* ! __TESTISEMPTY_H__ */
