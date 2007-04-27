606f8456ba10ac1a785ed18e84300854ba3bf732
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2007-04-27 06:24:50 +0000

    [RELEASE] Absolute Value

--- src/auxiliaryFunctions/includes/abs.h
+++ src/auxiliaryFunctions/includes/abs.h
@@ -5,7 +5,7 @@
 ** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
 **
 ** Started on  Thu Feb  8 10:12:17 2007 jofret
-** Last update Mon Apr 23 17:10:57 2007 jofret
+** Last update Wed Apr 25 11:26:42 2007 jofret
 **
 ** Copyright INRIA 2007
 */
@@ -38,16 +38,48 @@ double	dabss(double in);
  ** \brief Float Complex Absolute Value function
  ** Determine the absolute value of in.
  ** \param in : the float complex we must determine abs i.e. module.
- ** \return -in or in depending on the sign of in.
+ ** \return |in|.
  **/
 float	cabss(floatComplex in);
 
 /**
- ** \brief Double Absolute Value function
+ ** \brief Double Complex Absolute Value function
  ** Determine the absolute value of in.
- ** \param in : the double we must determine abs i.e. module.
- ** \return -in or +in depending on the abs of in.
+ ** \param in : the double complex we must determine abs i.e. module.
+ ** \return |in|.
  **/
 double	zabss(doubleComplex in);
+
+/**
+ ** \brief Float Array Absolute Value function
+ ** Determine the absolute value of in elements.
+ ** \param in : the float array we must determine abs.
+ ** \param out : the float array result.
+ **/
+void	sabsa(float *in, float* out, int size);
+
+/**
+ ** \brief Double Array Absolute Value function
+ ** Determine the absolute value of in elements.
+ ** \param in : the double array we must determine abs.
+ ** \param out : the double array result.
+ **/
+void	dabsa(double *in, double* out, int size);
+
+/**
+ ** \brief Float Complex Array Absolute Value function
+ ** Determine the absolute value of in elements.
+ ** \param in : the float complex array we must determine abs i.e. module.
+ ** \param out : the float complex array result i.e out[n] = |in[n]|.
+ **/
+void	cabsa(floatComplex *in, float* out, int size);
+
+/**
+ ** \brief Double Complex Array Absolute Value function
+ ** Determine the absolute value of in.
+ ** \param in : the double complex array we must determine abs i.e. module.
+ ** \param out : the double complex array result  i.e out[n] = |in[n]|.
+ **/
+void	zabsa(doubleComplex *in, double* out, int size);
 
 #endif /* !__ABS_H__ */
