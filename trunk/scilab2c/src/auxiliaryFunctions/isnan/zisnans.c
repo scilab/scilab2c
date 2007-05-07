7aced78302ca074121c207bd3f14d640d3fb93e8
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2007-05-07 14:12:10 +0000

    Is Nan : New function Must improve nan so far as complex numbers are concerned !

--- trunk/scilab2c/src/auxiliaryFunctions/isnan/zisnans.c
+++ trunk/scilab2c/src/auxiliaryFunctions/isnan/zisnans.c
@@ -1,1 +1,17 @@
-/* FIXME : Implementation */
+/*
+**  -*- C -*-
+**
+** zisnans.c
+** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
+**
+** Started on  Thu Apr 26 16:02:19 2007 jofret
+** Last update Thu Apr 26 17:36:44 2007 jofret
+**
+** Copyright INRIA 2007
+*/
+
+#include "isnan.h"
+
+bool	zisnans(doubleComplex in) {
+  return (isnan(zreals(in)) || isnan(zimags(in)));
+}
