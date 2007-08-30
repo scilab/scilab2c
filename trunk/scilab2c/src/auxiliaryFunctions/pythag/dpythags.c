10399c96b699fafe61a4c5208d17ea7d11f0a7e1
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2007-08-30 06:50:38 +0000

    Update pythag function

--- trunk/scilab2c/src/auxiliaryFunctions/pythag/dpythags.c
+++ trunk/scilab2c/src/auxiliaryFunctions/pythag/dpythags.c
@@ -5,13 +5,12 @@
 ** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
 **
 ** Started on  Mon Apr 23 11:54:15 2007 jofret
-** Last update Mon Apr 23 15:43:21 2007 jofret
+** Last update Thu Aug 16 12:10:17 2007 bruno
 **
 ** Copyright INRIA 2007
 */
 
 #include "pythag.h"
-#include "sqrt.h"
 
 double	dpythags(double x, double y) {
   return (dsqrts( (x*x) + (y*y) ) );
