bf42287da39e7c447b0140a866dacc242183a8be
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2007-10-22 15:24:30 +0000

    Moving array functions prototype

--- src/auxiliaryFunctions/rand/zranda.c
+++ src/auxiliaryFunctions/rand/zranda.c
@@ -1,1 +1,20 @@
-/* FIXME : Implementation */
+/*
+**  -*- C -*-
+**
+** zranda.c
+** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
+**
+** Started on  Fri Sep  7 16:08:40 2007 bruno
+** Last update Fri Sep  7 16:11:39 2007 bruno
+**
+** Copyright INRIA 2007
+*/
+
+#include "rand.h"
+
+void zranda(doubleComplex *out, int size) {
+  int i = 0;
+  for (i = 0 ; i < size ; ++i) {
+    out[i] = zrands();
+  }
+}
