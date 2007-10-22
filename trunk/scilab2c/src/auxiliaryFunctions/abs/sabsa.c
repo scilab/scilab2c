d1595513f6022cf43efadd255e6b153f71b878a7
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2007-10-22 15:22:10 +0000

    Moving array functions prototype

--- trunk/scilab2c/src/auxiliaryFunctions/abs/sabsa.c
+++ trunk/scilab2c/src/auxiliaryFunctions/abs/sabsa.c
@@ -5,14 +5,14 @@
 ** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
 **
 ** Started on  Wed Apr 25 10:53:09 2007 jofret
-** Last update Wed Apr 25 11:26:19 2007 jofret
+** Last update Mon Oct 22 10:45:22 2007 bruno
 **
 ** Copyright INRIA 2007
 */
 
 #include "abs.h"
 
-void	sabsa(float *in, float* out, int size) {
+void	sabsa(float *in, int size, float* out) {
   int i = 0;
   for (i = 0; i < size; ++i) {
     out[i] = sabss(in[i]);
