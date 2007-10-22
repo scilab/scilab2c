fece2516a1a7d15bc0340b01772b7602e8d778af
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2007-10-22 15:23:57 +0000

    Moving array functions prototype

--- trunk/scilab2c/src/auxiliaryFunctions/sign/csigna.c
+++ trunk/scilab2c/src/auxiliaryFunctions/sign/csigna.c
@@ -1,1 +1,24 @@
-/* FIXME : Implementation */
+/*
+**  -*- C -*-
+**
+** csigna.c
+** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
+**
+** Started on  Fri Sep  7 17:36:56 2007 bruno
+** Last update Fri Sep  7 17:37:15 2007 bruno
+**
+** Copyright INRIA 2007
+*/
+
+#include "sign.h"
+
+int csigna(floatComplex *in, int size) {
+  int i = 0;
+  int sign = 1;
+
+  /* FIXME : Does it what it's supposed to do ? */
+  for (i = 0 ; i < size ; ++i) {
+    sign = sign * csigns(in[i]);
+  }
+  return sign;
+}
--- trunk/scilab2c/src/auxiliaryFunctions/sign/zsigns.c
+++ trunk/scilab2c/src/auxiliaryFunctions/sign/zsigns.c
@@ -1,1 +1,18 @@
-/* FIXME : Implementation */
+/*
+**  -*- C -*-
+**
+** zsigns.c
+** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
+**
+** Started on  Fri Sep  7 17:29:33 2007 bruno
+** Last update Fri Sep  7 17:30:33 2007 bruno
+**
+** Copyright INRIA 2007
+*/
+
+#include "sign.h"
+
+int zsigns(doubleComplex in) {
+/* FIXME : Dummy... */
+  return -1;
+}
