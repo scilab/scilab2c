97e028ab189ab581675c212cc14df51c9a432b30
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2007-10-22 15:24:30 +0000

    Moving array functions prototype

--- scilab2c/src/auxiliaryFunctions/rand/srands.c
+++ scilab2c/src/auxiliaryFunctions/rand/srands.c
@@ -1,1 +1,18 @@
-/* FIXME : Implementation */
+/*
+**  -*- C -*-
+**
+** srands.c
+** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
+**
+** Started on  Mon May 14 16:22:52 2007 jofret
+** Last update Mon May 14 16:46:47 2007 jofret
+**
+** Copyright INRIA 2007
+*/
+
+#include "rand.h"
+
+float	srands(void) {
+  /* FIXME : need some code */
+  return 0;
+}
