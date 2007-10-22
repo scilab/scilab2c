de8854a17c89ae8bb27e26c48a7576595d85dd83
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2007-10-22 15:22:26 +0000

    Moving array functions prototype

--- scilab2c/src/auxiliaryFunctions/find/zfinda.c
+++ scilab2c/src/auxiliaryFunctions/find/zfinda.c
@@ -5,7 +5,7 @@
 ** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
 **
 ** Started on  Tue Feb 13 16:51:44 2007 jofret
-** Last update Tue Apr 24 18:12:37 2007 jofret
+** Last update Mon Oct 22 15:45:46 2007 bruno
 **
 ** Copyright INRIA 2007
 */
@@ -15,7 +15,7 @@
 int		zfinda(doubleComplex* z, int size) {
  int i = 0;
   for (i = 0; i < size ; ++i) {
-    if (zreals(z[i]) != 0 || zimags(z[i])) {
+    if (zreals(z[i]) != 0 || zimags(z[i]) != 0) {
       return i;
     }
   }
