30b92193758586c37ebfc84a695ed717c1575ed1
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2007-10-22 15:24:58 +0000

    Moving array functions prototype

--- trunk/scilab2c/src/auxiliaryFunctions/size/ssizea.c
+++ trunk/scilab2c/src/auxiliaryFunctions/size/ssizea.c
@@ -1,1 +1,18 @@
-/* FIXME : Implementation */
+/*
+**  -*- C -*-
+**
+** ssizea.c
+** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
+**
+** Started on  Mon Oct 22 10:47:17 2007 bruno
+** Last update Mon Oct 22 10:47:59 2007 bruno
+**
+** Copyright INRIA 2007
+*/
+
+#include "size.h"
+
+int ssizea(float *in, int size) {
+  /* FIXME : Implementation */
+  return 0;
+}
