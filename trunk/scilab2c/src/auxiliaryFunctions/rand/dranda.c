2a1017395f7dac227b1c779454a26fddae3e154b
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2007-10-22 15:24:30 +0000

    Moving array functions prototype

--- trunk/scilab2c/src/auxiliaryFunctions/rand/dranda.c
+++ trunk/scilab2c/src/auxiliaryFunctions/rand/dranda.c
@@ -1,1 +1,20 @@
-/* FIXME : Implementation */
+/*
+**  -*- C -*-
+**
+** dranda.c
+** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
+**
+** Started on  Fri Sep  7 15:55:47 2007 bruno
+** Last update Fri Sep  7 15:59:21 2007 bruno
+**
+** Copyright INRIA 2007
+*/
+
+#include "rand.h"
+
+void dranda(double *out, int size) {
+  int i = 0;
+  for (i = 0 ; i < size ; ++i) {
+    out[i] = drands();
+  }
+}
--- trunk/scilab2c/src/auxiliaryFunctions/rand/sranda.c
+++ trunk/scilab2c/src/auxiliaryFunctions/rand/sranda.c
@@ -1,1 +1,20 @@
-/* FIXME : Implementation */
+/*
+**  -*- C -*-
+**
+** sranda.c
+** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
+**
+** Started on  Fri Sep  7 15:52:34 2007 bruno
+** Last update Fri Sep  7 15:55:32 2007 bruno
+**
+** Copyright INRIA 2007
+*/
+
+#include "rand.h"
+
+void sranda(float *out, int size) {
+  int i = 0;
+  for (i = 0 ; i < size ; ++i) {
+    out[i] = srands();
+  }
+}
