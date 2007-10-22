26a5b193d959ced7ae9e3dd856ef093c46f206be
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2007-10-22 15:29:58 +0000

    C99 compliance, or even ISO C 89 one...

--- trunk/scilab2c/src/type/testFloatComplex.c
+++ trunk/scilab2c/src/type/testFloatComplex.c
@@ -5,7 +5,7 @@
 ** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
 **
 ** Started on  Thu Nov 30 16:59:04 2006 jofret
-** Last update Fri Mar 23 17:05:00 2007 jofret
+** Last update Mon Oct 22 10:22:19 2007 bruno
 **
 ** Copyright INRIA 2006
 */
@@ -19,19 +19,38 @@
 
 int main(int argc, char **argv) {
   /* z = -3 + 25*%i */
-  floatComplex z = FloatComplex((float)-3, (float)25);
-  printf("Partie reelle = %f\n", creals(z));
-  assert(creals(z) == (float)-3.0);
-  printf("Partie imaginaire = %f\n", cimags(z));
-  assert(cimags(z) == (float)25);
+  floatComplex z = FloatComplex((float)3, (float)-25);
+  /* y = -3.123456 + 25.123456*%i */
+  floatComplex y = FloatComplex((float)-3.123,(float)25.123);
+  /* t = z + y */
+  floatComplex t = cadds(z,y);
+  /* u = z - y */
+  floatComplex u = cdiffs(z,y);
 
-  /* z = -3.123456 + 25.123456*%i */
-  z = FloatComplex((float)-3.123456,(float)25.123456);
-  printf("Partie reelle = %f\n", creals(z));
-  assert(creals(z) == (float)-3.123456);
-  printf("Partie imaginaire = %f\n", cimags(z));
-  assert(cimags(z) == (float)25.123456);
 
+  /* z = -3 + 25*%i */
+  printf("Partie reelle = %f\n", creals(z));
+  assert(creals(z) == (float)3);
+  printf("Partie imaginaire = %f\n", cimags(z));
+  assert(cimags(z) == (float)-25);
+
+  /* y = -3.123456 + 25.123456*%i */
+  printf("Partie reelle = %f\n", creals(y));
+  assert(creals(y) == (float)-3.123);
+  printf("Partie imaginaire = %f\n", cimags(y));
+  assert(cimags(y) == (float)25.123);
+
+  /* Try to have somme addition */
+  printf("Partie reelle = %f\n", creals(t));
+  assert(creals(t) == (float)3 + (float)-3.123);
+  printf("Partie imaginaire = %f\n", cimags(t));
+  assert(cimags(t) == (float)-25 + (float)25.123);
+
+  /* Try to have somme diff */
+  printf("Partie reelle = %f\n", creals(u));
+  assert(creals(u) == (float)3 - (float)-3.123);
+  printf("Partie imaginaire = %f\n", cimags(u));
+  assert(cimags(u) == (float)-25 - (float)25.123);
 
   return 0;
 }
