9923a0000edc9f6bcb54566996c35bcdec9015ff
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2007-10-22 15:20:53 +0000

    Moving prototype

--- trunk/scilab2c/src/auxiliaryFunctions/includes/sign.h
+++ trunk/scilab2c/src/auxiliaryFunctions/includes/sign.h
@@ -5,7 +5,7 @@
 ** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
 **
 ** Started on  Thu Feb  8 10:12:17 2007 jofret
-** Last update Tue Mar 20 14:58:52 2007 jofret
+** Last update Fri Sep  7 17:37:57 2007 bruno
 **
 ** Copyright INRIA 2007
 */
@@ -14,6 +14,9 @@
 #define __SIGN_H__
 
 #include <math.h>
+
+#include "floatComplex.h"
+#include "doubleComplex.h"
 
 /**
  ** \brief Float Signe function
@@ -30,5 +33,57 @@ int	ssigns(float in);
  ** \return -1 or +1 depending on the sign of in.
  **/
 int	dsigns(double in);
+
+/**
+ ** \brief Float Complex Signe function
+ ** Determine the sine of in (assume that 0 is positive).
+ ** \param in : the float we must determine sign.
+ ** \return -1 or +1 depending on the sign of in.
+ **/
+int	csigns(floatComplex in);
+
+/**
+ ** \brief Double Complex Signe function
+ ** Determine the sine of in (assume that 0 is positive).
+ ** \param in : the double we must determine sign.
+ ** \return -1 or +1 depending on the sign of in.
+ **/
+int	zsigns(doubleComplex in);
+
+/**
+ ** \brief Float Signe Array function
+ ** Determine the sine of an array in (assume that 0 is positive).
+ ** \param in : the float array we must determine sign.
+ ** \param size : the number of elements.
+ ** \return -1 or +1 depending on the sign of in elements.
+ **/
+int	ssigna(float *in, int size);
+
+/**
+ ** \brief Double Signe Array function
+ ** Determine the sine of an array in (assume that 0 is positive).
+ ** \param in : the double array we must determine sign.
+ ** \param size : the number of elements.
+ ** \return -1 or +1 depending on the sign of in elements.
+ **/
+int	dsigna(double *in, int size);
+
+/**
+ ** \brief Float Signe Complex Array function
+ ** Determine the sine of an array in (assume that 0 is positive).
+ ** \param in : the float complex array we must determine sign.
+ ** \param size : the number of elements.
+ ** \return -1 or +1 depending on the sign of in elements.
+ **/
+int	csigna(floatComplex *in, int size);
+
+/**
+ ** \brief Double Signe Complex Array function
+ ** Determine the sine of an array in (assume that 0 is positive).
+ ** \param in : the double complex array we must determine sign.
+ ** \param size : the number of elements.
+ ** \return -1 or +1 depending on the sign of in elements.
+ **/
+int	zsigna(doubleComplex *in, int size);
 
 #endif /* !__SIGN_H__ */
