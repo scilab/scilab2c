0601a3c045b2575d6f9fb69155e3d32cdfc13850
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2008-04-11 12:53:50 +0000

    add tests and mimic Scilab behaviour sign(0) = 0

--- scilab2c/src/auxiliaryFunctions/sign/zsigns.c
+++ scilab2c/src/auxiliaryFunctions/sign/zsigns.c
@@ -1,18 +1,18 @@
 /*
-**  -*- C -*-
-**
-** zsigns.c
-** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
-**
-** Started on  Fri Sep  7 17:29:33 2007 bruno
-** Last update Fri Sep  7 17:30:33 2007 bruno
-**
-** Copyright INRIA 2007
-*/
+ *  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ *  Copyright (C) 2007-2008 - INRIA - Bruno JOFRET
+ *
+ *  This file must be used under the terms of the CeCILL.
+ *  This source file is licensed as described in the file COPYING, which
+ *  you should have received as part of this distribution.  The terms
+ *  are also available at
+ *  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
 
 #include "sign.h"
+#include "abs.h"
 
-int zsigns(doubleComplex in) {
-/* FIXME : Dummy... */
-  return -1;
+doubleComplex zsigns(doubleComplex in) {
+  return DoubleComplex(zreals(in) / zabss(in), zimags(in) / zabss(in));
 }
