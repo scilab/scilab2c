bfef29f4a976fc12b3ec43d21e8452d7979744ff
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2008-04-11 12:00:21 +0000

    Matrixes operations : Must be linked to  LAPAACK soon

---
+++ src/matrixOperations/matrixAddition.h
@@ -0,0 +1,74 @@
+/*
+ *  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ *  Copyright (C) 2007-2008 - INRIA - Bruno JOFRET
+ *
+ *  This file must be used under the terms of the CeCILL.
+ *  This source file is licensed as described in the file COPYING, which
+ *  you should have received as part of this distribution.  The terms
+ *  are also available at
+ *  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
+
+#ifndef __MATRICXADDITION_H__
+#define __MATRICXADDITION_H__
+
+#include "floatComplex.h"
+#include "doubleComplex.h"
+
+/*
+**
+** WARNING WE ASSUME MATRIXES TO BE CONSCISTENT
+** size1 = size2;
+**
+*/
+
+
+/*
+** \brief Compute an addition element ways for floats.
+** \param in1 : input array.
+** \param size1 : size of in1 array.
+** \param in2 : input arry.
+** \param size2 : size of in2 array.
+** \param out : array that contains the addition in1 + in2.
+*/
+void	saddma(float *in1, int size1,
+	       float *in2, int size2,
+	       float *out);
+/*
+** \brief Compute an addition element ways for double.
+** \param in1 : input array.
+** \param size1 : size of in1 array.
+** \param in2 : input arry.
+** \param size2 : size of in2 array.
+** \param out : array that contains the addition in1 + in2.
+*/
+void	daddma(double *in1, int size1,
+	       double *in2, int size2,
+	       double * out);
+
+/*
+** \brief Compute an addition element ways for complex single precision.
+** \param in1 : input array.
+** \param size1 : size of in1 array.
+** \param in2 : input arry.
+** \param size2 : size of in2 array.
+** \param out : array that contains the addition in1 + in2.
+*/
+void	caddma(floatComplex *in1, int size1,
+	       floatComplex *in2, int size2,
+	       floatComplex *out);
+
+/*
+** \brief Compute an addition element ways for complex double precision.
+** \param in1 : input array.
+** \param size1 : size of in1 array.
+** \param in2 : input arry.
+** \param size2 : size of in2 array.
+** \param out : array that contains the addition in1 + in2.
+*/
+void	zaddma(doubleComplex *in1, int size1,
+	       doubleComplex *in2, int size2,
+	       doubleComplex *out);
+
+#endif /* !__MATRICXADDITION_H__ */
---
+++ src/matrixOperations/matrixSubtraction.c
@@ -0,0 +1,93 @@
+/*
+ *  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ *  Copyright (C) 2008-2008 - INRIA - Bruno JOFRET
+ *
+ *  This file must be used under the terms of the CeCILL.
+ *  This source file is licensed as described in the file COPYING, which
+ *  you should have received as part of this distribution.  The terms
+ *  are also available at
+ *  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
+
+#include "matrixSubtraction.h"
+
+/*
+** \brief Compute an substraction element ways for floats.
+** \param in1 : input array.
+** \param size1 : size of in1 array.
+** \param in2 : input arry.
+** \param size2 : size of in2 array.
+** \param out : array that contains in1 - in2.
+*/
+void	sdiffma(float *in1, int size1,
+	       float *in2, int size2,
+	       float *out)
+{
+  int i = 0;
+
+  for(i = 0 ; i < size1 && i < size2 ; ++i)
+    {
+      out[i] = in1[i] - in2[i];
+    }
+}
+
+/*
+** \brief Compute an substraction element ways for double.
+** \param in1 : input array.
+** \param size1 : size of in1 array.
+** \param in2 : input arry.
+** \param size2 : size of in2 array.
+** \param out : array that contains in1 - in2.
+*/
+void	ddiffma(double *in1, int size1,
+	       double *in2, int size2,
+	       double * out)
+{
+  int i = 0;
+
+  for(i = 0 ; i < size1 && i < size2 ; ++i)
+    {
+      out[i] = in1[i] - in2[i];
+    }
+}
+
+/*
+** \brief Compute an substraction element ways for complex single precision.
+** \param in1 : input array.
+** \param size1 : size of in1 array.
+** \param in2 : input arry.
+** \param size2 : size of in2 array.
+** \param out : array that contains in1 - in2.
+*/
+void	cdiffma(floatComplex *in1, int size1,
+	       floatComplex *in2, int size2,
+	       floatComplex *out)
+{
+  int i = 0;
+
+  for(i = 0 ; i < size1 && i < size2 ; ++i)
+    {
+      out[i] = cdiffs(in1[i], in2[i]);
+    }
+}
+
+/*
+** \brief Compute an substraction element ways for complex double precision.
+** \param in1 : input array.
+** \param size1 : size of in1 array.
+** \param in2 : input arry.
+** \param size2 : size of in2 array.
+** \param out : array that contains in1 - in2.
+*/
+void	zdiffma(doubleComplex *in1, int size1,
+	       doubleComplex *in2, int size2,
+	       doubleComplex *out)
+{
+  int i = 0;
+
+  for(i = 0 ; i < size1 && i < size2 ; ++i)
+    {
+      out[i] = zdiffs(in1[i], in2[i]);
+    }
+}
