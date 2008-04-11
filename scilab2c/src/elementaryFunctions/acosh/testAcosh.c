d4159bab857eaa612e523037565d0c2032609e92
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2008-04-11 11:37:42 +0000

    improve test + libtool management

--- scilab2c/src/elementaryFunctions/acosh/testAcosh.c
+++ scilab2c/src/elementaryFunctions/acosh/testAcosh.c
@@ -1,14 +1,14 @@
 /*
-**  -*- C -*-
-**
-** testAcosh.c
-** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
-**
-** Started on  Fri Dec  8 15:05:44 2006 jofret
-** Last update Mon Oct 22 11:58:49 2007 bruno
-**
-** Copyright INRIA 2006
-*/
+ *  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ *  Copyright (C) 2006-2008 - INRIA - Bruno JOFRET
+ *
+ *  This file must be used under the terms of the CeCILL.
+ *  This source file is licensed as described in the file COPYING, which
+ *  you should have received as part of this distribution.  The terms
+ *  are also available at
+ *  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
 
 #include "testAcosh.h"
 
