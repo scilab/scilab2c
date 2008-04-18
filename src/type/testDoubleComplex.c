e15d2967125c1663ca83ec8ddb0bc23e481fef52
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2008-04-18 08:42:16 +0000

    Enable warnings

--- src/type/testDoubleComplex.c
+++ src/type/testDoubleComplex.c
@@ -1,20 +1,20 @@
 /*
-**  -*- C -*-
-**
-** main.c
-** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
-**
-** Started on  Thu Nov 30 16:59:04 2006 jofret
-** Last update Mon Oct 22 10:21:07 2007 bruno
-**
-** Copyright INRIA 2006
-*/
+ *  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ *  Copyright (C) 2006-2008 - INRIA - Bruno JOFRET
+ *
+ *  This file must be used under the terms of the CeCILL.
+ *  This source file is licensed as described in the file COPYING, which
+ *  you should have received as part of this distribution.  The terms
+ *  are also available at
+ *  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
 
 #include <stdio.h>
 #include <assert.h>
 #include "doubleComplex.h"
 
-int main(int argc, char **argv) {
+int main(void) {
   /* z = -3 + 25*%i */
   doubleComplex z = DoubleComplex(3,-25);
   /* y = -3.123456 + 25.123456*%i */
--- src/type/testFloatComplex.c
+++ src/type/testFloatComplex.c
@@ -1,14 +1,14 @@
 /*
-**  -*- C -*-
-**
-** main.c
-** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
-**
-** Started on  Thu Nov 30 16:59:04 2006 jofret
-** Last update Mon Oct 22 10:22:19 2007 bruno
-**
-** Copyright INRIA 2006
-*/
+ *  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ *  Copyright (C) 2006-2008 - INRIA - Bruno JOFRET
+ *
+ *  This file must be used under the terms of the CeCILL.
+ *  This source file is licensed as described in the file COPYING, which
+ *  you should have received as part of this distribution.  The terms
+ *  are also available at
+ *  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
 
 #include <stdio.h>
 #include <assert.h>
@@ -17,7 +17,7 @@
 #include "floatComplex.h"
 
 
-int main(int argc, char **argv) {
+int main(void) {
   /* z = -3 + 25*%i */
   floatComplex z = FloatComplex((float)3, (float)-25);
   /* y = -3.123456 + 25.123456*%i */
