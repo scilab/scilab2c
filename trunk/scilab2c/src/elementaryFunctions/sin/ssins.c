5e3a0db817ab0093f0160d3a8a7ef2765afadf05
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2008-04-18 09:38:20 +0000

    Double vs Float better management. Enable psycho warnings.

--- trunk/scilab2c/src/elementaryFunctions/sin/ssins.c
+++ trunk/scilab2c/src/elementaryFunctions/sin/ssins.c
@@ -1,18 +1,18 @@
 /*
-**  -*- C -*-
-**
-** ssins.c
-** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
-**
-** Started on  Fri Dec  8 10:45:34 2006 jofret
-** Last update Fri Apr 20 14:39:20 2007 jofret
-**
-** Copyright INRIA 2006
-*/
+ *  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ *  Copyright (C) 2008-2008 - INRIA - Bruno JOFRET
+ *
+ *  This file must be used under the terms of the CeCILL.
+ *  This source file is licensed as described in the file COPYING, which
+ *  you should have received as part of this distribution.  The terms
+ *  are also available at
+ *  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
 
 #include <math.h>
 #include "sin.h"
 
 float ssins(float x) {
-  return (sin(x));
+  return (sinf(x));
 }
