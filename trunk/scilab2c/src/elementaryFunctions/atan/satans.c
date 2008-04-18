be558d3137853f2b6feacb8720d1130eb453f8aa
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2008-04-18 15:01:47 +0000

    Double vs Float better management. Enable psycho warnings.

--- trunk/scilab2c/src/elementaryFunctions/atan/satans.c
+++ trunk/scilab2c/src/elementaryFunctions/atan/satans.c
@@ -1,18 +1,18 @@
 /*
-**  -*- C -*-
-**
-** satans.c
-** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
-**
-** Started on  Thu Dec  7 11:05:37 2006 jofret
-** Last update Thu Sep  6 11:53:39 2007 bruno
-**
-** Copyright INRIA 2006
-*/
+ *  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ *  Copyright (C) 2008-2008 - INRIA - Bruno JOFRET
+ *
+ *  This file must be used under the terms of the CeCILL.
+ *  This source file is licensed as described in the file COPYING, which
+ *  you should have received as part of this distribution.  The terms
+ *  are also available at
+ *  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
 
 #include <math.h>
 #include "atan.h"
 
 float		satans(float x) {
-  return (atan(x));
+  return (atanf(x));
 }
