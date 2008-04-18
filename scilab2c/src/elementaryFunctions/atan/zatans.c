d67f2fd3d8555dd008ed6bf739944c383877c430
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2008-04-18 15:01:47 +0000

    Double vs Float better management. Enable psycho warnings.

--- scilab2c/src/elementaryFunctions/atan/zatans.c
+++ scilab2c/src/elementaryFunctions/atan/zatans.c
@@ -1,18 +1,18 @@
 /*
-**  -*- C -*-
-**
-** zatans.c
-** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
-**
-** Started on  Thu Dec  7 12:05:48 2006 jofret
-** Last update Thu Sep  6 11:53:07 2007 bruno
-**
-** Copyright INRIA 2006
-*/
+ *  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ *  Copyright (C) 2006-2008 - INRIA - Bruno JOFRET
+ *
+ *  This file must be used under the terms of the CeCILL.
+ *  This source file is licensed as described in the file COPYING, which
+ *  you should have received as part of this distribution.  The terms
+ *  are also available at
+ *  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
 
 #include "atan.h"
 
 doubleComplex		zatans(doubleComplex z) {
   /* FIXME: Dummy... */
-  return (DoubleComplex(0,1));
+  return z;
 }
