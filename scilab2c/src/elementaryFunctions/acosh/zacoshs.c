568b56ac2ab2f90a2f38e8236742df8ddde8be37
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2008-04-18 09:28:14 +0000

    Double vs Float better management. Enable psycho warnings.

--- scilab2c/src/elementaryFunctions/acosh/zacoshs.c
+++ scilab2c/src/elementaryFunctions/acosh/zacoshs.c
@@ -1,18 +1,18 @@
 /*
-**  -*- C -*-
-**
-** zacoshs.c
-** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
-**
-** Started on  Fri Jan  5 10:24:38 2007 jofret
-** Last update Thu Sep  6 10:42:12 2007 bruno
-**
-** Copyright INRIA 2007
-*/
+ *  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ *  Copyright (C) 2007-2008 - INRIA - Bruno JOFRET
+ *
+ *  This file must be used under the terms of the CeCILL.
+ *  This source file is licensed as described in the file COPYING, which
+ *  you should have received as part of this distribution.  The terms
+ *  are also available at
+ *  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
 
 #include "acosh.h"
 
 doubleComplex		zacoshs(doubleComplex z) {
   /* FIXME: Dummy... */
-  return (DoubleComplex(0,1));
+  return z;
 }
