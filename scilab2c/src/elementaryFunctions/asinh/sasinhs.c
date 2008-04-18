2992a71858f5827c95497f29b85e1b44f1eea85c
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2008-04-18 14:01:15 +0000

    Double vs Float better management. Enable psycho warnings.

--- scilab2c/src/elementaryFunctions/asinh/sasinhs.c
+++ scilab2c/src/elementaryFunctions/asinh/sasinhs.c
@@ -1,18 +1,18 @@
 /*
-**  -*- C -*-
-**
-** sasinhs.c
-** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
-**
-** Started on  Fri Jan  5 10:25:44 2007 jofret
-** Last update Thu Sep  6 11:21:04 2007 bruno
-**
-** Copyright INRIA 2007
-*/
+ *  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ *  Copyright (C) 2008-2008 - INRIA - Bruno JOFRET
+ *
+ *  This file must be used under the terms of the CeCILL.
+ *  This source file is licensed as described in the file COPYING, which
+ *  you should have received as part of this distribution.  The terms
+ *  are also available at
+ *  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
 
 #include <math.h>
 #include "asinh.h"
 
 float		sasinhs(float x) {
-  return (asinh(x));
+  return (asinhf(x));
 }
