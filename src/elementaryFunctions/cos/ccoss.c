f7ae4cce0bc844b137ccce135578c2248a6642b1
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2008-04-18 09:10:33 +0000

    Double vs Float better management. Enable psycho warnings.

--- src/elementaryFunctions/cos/ccoss.c
+++ src/elementaryFunctions/cos/ccoss.c
@@ -1,14 +1,14 @@
 /*
-**  -*- C -*-
-**
-** ccoss.c
-** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
-**
-** Started on  Thu Dec  7 12:04:28 2006 jofret
-** Last update Fri Feb 23 16:35:12 2007 jofret
-**
-** Copyright INRIA 2006
-*/
+ *  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ *  Copyright (C) 2008-2008 - INRIA - Bruno JOFRET
+ *
+ *  This file must be used under the terms of the CeCILL.
+ *  This source file is licensed as described in the file COPYING, which
+ *  you should have received as part of this distribution.  The terms
+ *  are also available at
+ *  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
 
 #include "cos.h"
 #include "sin.h"
--- src/elementaryFunctions/cos/scoss.c
+++ src/elementaryFunctions/cos/scoss.c
@@ -1,18 +1,18 @@
 /*
-**  -*- C -*-
-**
-** scoss.c
-** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
-**
-** Started on  Thu Dec  7 11:05:37 2006 jofret
-** Last update Fri Apr 20 14:36:40 2007 jofret
-**
-** Copyright INRIA 2006
-*/
+ *  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ *  Copyright (C) 2008-2008 - INRIA - Bruno JOFRET
+ *
+ *  This file must be used under the terms of the CeCILL.
+ *  This source file is licensed as described in the file COPYING, which
+ *  you should have received as part of this distribution.  The terms
+ *  are also available at
+ *  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
 
 #include <math.h>
 #include "cos.h"
 
 float		scoss(float x) {
-  return (cos(x));
+  return (cosf(x));
 }
