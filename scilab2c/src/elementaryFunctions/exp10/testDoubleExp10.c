3b6e5d7009c755a854ae8ee31d325b444ace3d7e
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2008-04-21 09:17:16 +0000

    Double vs Float better management. Enable psycho warnings.

---
+++ scilab2c/src/elementaryFunctions/exp10/testDoubleExp10.c
@@ -0,0 +1,45 @@
+/*
+ *  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ *  Copyright (C) 2008-2008 - INRIA - Bruno JOFRET
+ *
+ *  This file must be used under the terms of the CeCILL.
+ *  This source file is licensed as described in the file COPYING, which
+ *  you should have received as part of this distribution.  The terms
+ *  are also available at
+ *  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
+
+#include "testExp10.h"
+
+void dexp10sTest(void) {
+  printf(">> Double scalar\n");
+  printf("dexp10s(0) = %e\n", dexp10s(0.0));
+  printf("dexp10s(PI) = %e\n", dexp10s(DPI));
+  printf("dexp10s(PI/2) = %e\n", dexp10s(DPI/2));
+  printf("dexp10s(PI/3) = %e\n", dexp10s(DPI/3));
+  printf("dexp10s(PI/4) = %e\n", dexp10s(DPI/4));
+  printf("dexp10s(PI/6) = %e\n", dexp10s(DPI/6));
+  printf("dexp10s(-PI) = %e\n", dexp10s(-DPI));
+  printf("dexp10s(-PI/2) = %e\n", dexp10s(-DPI/2));
+  printf("dexp10s(-PI/3) = %e\n", dexp10s(-DPI/3));
+  printf("dexp10s(-PI/4) = %e\n", dexp10s(-DPI/4));
+  printf("dexp10s(-PI/6) = %e\n", dexp10s(-DPI/6));
+}
+
+int testExp10(void) {
+  printf("\n>>>> Double 10-Base Exponential Tests\n");
+  dexp10sTest();
+  /* FIXME : Implement some test here ... */
+  /*
+    zexp10sTest();
+    dexp10aTest();
+    zexp10aTest();
+  */
+  return 0;
+}
+
+int main(void) {
+  assert(testExp10() == 0);
+  return 0;
+}
