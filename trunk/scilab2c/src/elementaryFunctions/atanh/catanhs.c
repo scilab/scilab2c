0676b48eb210d2d2d21525d598ea81bee10d98be
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2008-04-21 08:32:37 +0000

    Double vs Float better management. Enable psycho warnings.

--- trunk/scilab2c/src/elementaryFunctions/atanh/catanhs.c
+++ trunk/scilab2c/src/elementaryFunctions/atanh/catanhs.c
@@ -1,22 +1,18 @@
 /*
-**  -*- C -*-
-**
-** catanhs.c
-** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
-**
-** Started on  Thu Dec  7 12:04:28 2006 jofret
-** Last update Thu Sep  6 15:12:27 2007 bruno
-**
-** Copyright INRIA 2006
-*/
+ *  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ *  Copyright (C) 2006-2008 - INRIA - Bruno JOFRET
+ *
+ *  This file must be used under the terms of the CeCILL.
+ *  This source file is licensed as described in the file COPYING, which
+ *  you should have received as part of this distribution.  The terms
+ *  are also available at
+ *  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
 
 #include "atanh.h"
 
 floatComplex		catanhs(floatComplex z) {
-
-  double out_real = 0;
-  double out_imag = 0;
   /* FIXME : Let's code...  */
-
-  return FloatComplex(out_real, out_imag);
+  return z;
 }
