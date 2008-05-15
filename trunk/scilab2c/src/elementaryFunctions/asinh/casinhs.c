bcc7c2be023fa5043f2ee222531593786f6a5ffe
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2008-05-15 09:45:12 +0000

    * ASINH Mapped to emulate scilab behaviour   this mean it does not call the C library if scilab   use another algorithm instead.

--- trunk/scilab2c/src/elementaryFunctions/asinh/casinhs.c
+++ trunk/scilab2c/src/elementaryFunctions/asinh/casinhs.c
@@ -1,18 +1,26 @@
 /*
-**  -*- C -*-
-**
-** casinhs.c
-** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
-**
-** Started on  Fri Jan  5 11:29:45 2007 jofret
-** Last update Thu Sep  6 11:23:00 2007 bruno
-**
-** Copyright INRIA 2007
-*/
+ *  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ *  Copyright (C) 2007-2008 - INRIA - Bruno JOFRET
+ *
+ *  This file must be used under the terms of the CeCILL.
+ *  This source file is licensed as described in the file COPYING, which
+ *  you should have received as part of this distribution.  The terms
+ *  are also available at
+ *  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
+
+//
+//  METHOD
+//     based on the formula  asinh(z) = -i asin(i z)
+//
 
 #include "asinh.h"
+#include "asin.h"
 
 floatComplex		casinhs(floatComplex z) {
-  /* FIXME: Dummy... */
-  return z;
+  floatComplex i_z = FloatComplex(-cimags(z), creals(z));
+  floatComplex asin_i_z = casins(i_z);
+
+  return FloatComplex(cimags(asin_i_z), creals(asin_i_z));
 }
