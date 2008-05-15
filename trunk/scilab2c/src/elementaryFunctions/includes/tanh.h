4825d51461558298bb42f55c54bd215a6b6a9e6a
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2008-05-15 09:49:57 +0000

    License update

--- trunk/scilab2c/src/elementaryFunctions/includes/tanh.h
+++ trunk/scilab2c/src/elementaryFunctions/includes/tanh.h
@@ -1,14 +1,14 @@
 /*
-**  -*- C -*-
-**
-** tanh.h
-** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
-**
-** Started on  Tue Dec  5 15:49:18 2006 jofret
-** Last update Mon Oct 22 08:55:49 2007 bruno
-**
-** Copyright INRIA 2006
-*/
+ *  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ *  Copyright (C) 2006-2008 - INRIA - Bruno JOFRET
+ *
+ *  This file must be used under the terms of the CeCILL.
+ *  This source file is licensed as described in the file COPYING, which
+ *  you should have received as part of this distribution.  The terms
+ *  are also available at
+ *  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
 
 #ifndef __TANH_H__
 #define __TANH_H__
