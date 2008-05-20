318a2c345b386d92ab977f4c1e33e2a002fa50f3
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2008-05-20 07:26:09 +0000

    * Importing Scilab ATAN algorithm * License update * Tests

--- scilab2c/src/elementaryFunctions/atan/zatana.c
+++ scilab2c/src/elementaryFunctions/atan/zatana.c
@@ -1,14 +1,14 @@
 /*
-**  -*- C -*-
-**
-** zatana.c
-** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
-**
-** Started on  Thu Dec  7 16:12:02 2006 jofret
-** Last update Mon Oct 22 09:56:00 2007 bruno
-**
-** Copyright INRIA 2006
-*/
+ *  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ *  Copyright (C) 2006-2008 - INRIA - Bruno JOFRET
+ *
+ *  This file must be used under the terms of the CeCILL.
+ *  This source file is licensed as described in the file COPYING, which
+ *  you should have received as part of this distribution.  The terms
+ *  are also available at
+ *  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
 
 #include "atan.h"
 
