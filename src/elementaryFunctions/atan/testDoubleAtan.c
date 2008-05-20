54c5df264795ceb76bba56b03cafe01578452608
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2008-05-20 07:26:09 +0000

    * Importing Scilab ATAN algorithm * License update * Tests

--- src/elementaryFunctions/atan/testDoubleAtan.c
+++ src/elementaryFunctions/atan/testDoubleAtan.c
@@ -27,15 +27,59 @@ void datansTest() {
   printf("datans(-PI/6) = %e\n", datans(-DPI/6));
 }
 
+void zatansTest() {
+ doubleComplex pi_pi = DoubleComplex(DPI, DPI);
+  doubleComplex pi_2_pi_2 = DoubleComplex(DPI/2, DPI/2);
+  doubleComplex pi_2_pi_3 = DoubleComplex(DPI/2, DPI/3);
+  doubleComplex pi_2_pi_4 = DoubleComplex(DPI/2, DPI/4);
+  doubleComplex out;
+
+  printf(">> Double Complex scalar\n");
+  out = zatans(pi_pi);
+  printf("zatans(PI + I*PI) = %e + I * %e\n", zreals(out), zimags(out));
+  out = zatans(pi_2_pi_2);
+  printf("zatans(PI/2 + I*PI/2) = %e + I * %e\n", zreals(out), zimags(out));
+  out = zatans(pi_2_pi_3);
+  printf("zatans(PI/2 + I*PI/3) = %e + I * %e\n", zreals(out), zimags(out));
+  out = zatans(pi_2_pi_4);
+  printf("zatans(PI/2 + I*PI/4) = %e + I * %e\n", zreals(out), zimags(out));
+}
+
+void datanaTest(void) {
+  double out[5];
+  double in[5] = {DPI, DPI/2, DPI/3, DPI/4, DPI/6};
+  int i = 0;
+
+  printf(">> Double Array\n");
+  datana(in, 5, out);
+  for (i = 0 ; i < 5 ; ++i)
+    printf("satana(array) = %f\n", out[i]);
+
+}
+
+void zatanaTest(void) {
+  doubleComplex in[4];
+  doubleComplex out[4];
+  int i = 0;
+
+  in[0] = DoubleComplex(DPI, DPI);
+  in[1] = DoubleComplex(DPI/2, DPI/2);
+  in[2] = DoubleComplex(DPI/2, DPI/3);
+  in[3] = DoubleComplex(DPI/2, DPI/4);
+
+  zatana(in, 4, out);
+  printf(">> Double Complex Array\n");
+  for (i = 0 ; i < 4 ; ++i)
+    printf("zatana(array) = %e + I * %e\n", zreals(out[i]), zimags(out[i]));
+}
+
 int testAtan() {
   printf("\n>>>> Double Arc Tangeant Tests\n");
   datansTest();
-  /* FIXME : Implement some test here ... */
-  /*
-    zatansTest();
-    datanaTest();
-    zatanaTest();
-  */
+  zatansTest();
+  datanaTest();
+  zatanaTest();
+
   return 0;
 }
 
--- src/elementaryFunctions/atan/testFloatAtan.c
+++ src/elementaryFunctions/atan/testFloatAtan.c
@@ -27,15 +27,58 @@ void satansTest() {
   printf("satans(-PI/6) = %f\n", satans(-FPI/6));
 }
 
+void catansTest(void) {
+  floatComplex pi_pi = FloatComplex(FPI, FPI);
+  floatComplex pi_2_pi_2 = FloatComplex(FPI/2, FPI/2);
+  floatComplex pi_2_pi_3 = FloatComplex(FPI/2, FPI/3);
+  floatComplex pi_2_pi_4 = FloatComplex(FPI/2, FPI/4);
+  floatComplex out;
+
+  printf(">> Float Complex scalar\n");
+  out = catans(pi_pi);
+  printf("catans(PI + I*PI) = %e + I * %e\n", creals(out), cimags(out));
+  out = catans(pi_2_pi_2);
+  printf("catans(PI/2 + I*PI/2) = %e + I * %e\n", creals(out), cimags(out));
+  out = catans(pi_2_pi_3);
+  printf("catans(PI/2 + I*PI/3) = %e + I * %e\n", creals(out), cimags(out));
+  out = catans(pi_2_pi_4);
+  printf("catans(PI/2 + I*PI/4) = %e + I * %e\n", creals(out), cimags(out));
+}
+
+void satanaTest(void) {
+  float out[5];
+  float in[5] = {FPI, FPI/2, FPI/3, FPI/4, FPI/6};
+  int i = 0;
+
+  printf(">> Float Array\n");
+  satana(in, 5, out);
+  for (i = 0 ; i < 5 ; ++i)
+    printf("satana(array) = %f\n", out[i]);
+
+}
+
+void catanaTest(void) {
+  floatComplex in[4];
+  floatComplex out[4];
+  int i = 0;
+
+  in[0] = FloatComplex(FPI, FPI);
+  in[1] = FloatComplex(FPI/2, FPI/2);
+  in[2] = FloatComplex(FPI/2, FPI/3);
+  in[3] = FloatComplex(FPI/2, FPI/4);
+
+  catana(in, 4, out);
+  printf(">> Float Complex Array\n");
+  for (i = 0 ; i < 4 ; ++i)
+    printf("catana(array) = %e + I * %e\n", creals(out[i]), cimags(out[i]));
+}
 int testAtan() {
   printf("\n>>>> Float Arc tangeant Tests\n");
   satansTest();
-  /* FIXME : Implement some test here ... */
-  /*
-    catansTest();
-    satanaTest();
-    catanaTest();
-  */
+  catansTest();
+  satanaTest();
+  catanaTest();
+
   return 0;
 }
 
