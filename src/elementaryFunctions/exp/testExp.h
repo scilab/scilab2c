2f6a3cf7733f3680dcaea87c8200da19ce843c70
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2008-05-21 11:44:52 +0000

    * Exp Complex Algorithm update according Scilab one * License update

--- src/elementaryFunctions/exp/testExp.h
+++ src/elementaryFunctions/exp/testExp.h
@@ -1,14 +1,14 @@
 /*
-**  -*- C -*-
-**
-** testExp.h
-** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
-**
-** Started on  Wed Apr 18 17:07:03 2007 jofret
-** Last update Wed Apr 18 17:07:43 2007 jofret
-**
-** Copyright INRIA 2007
-*/
+ *  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ *  Copyright (C) 2007-2008 - INRIA - Bruno JOFRET
+ *
+ *  This file must be used under the terms of the CeCILL.
+ *  This source file is licensed as described in the file COPYING, which
+ *  you should have received as part of this distribution.  The terms
+ *  are also available at
+ *  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
 
 #ifndef _TESTEXP_H_
 #define _TESTEXP_H_
