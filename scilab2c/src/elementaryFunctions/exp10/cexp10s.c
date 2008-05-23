baa2baac61eb8d91df31be1d57f4ed7d9cb92f8e
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2008-05-23 12:48:40 +0000

    Correct dependancies

--- scilab2c/src/elementaryFunctions/exp10/cexp10s.c
+++ scilab2c/src/elementaryFunctions/exp10/cexp10s.c
@@ -1,14 +1,14 @@
 /*
-**  -*- C -*-
-**
-** cexp10s.c
-** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
-**
-** Started on  Thu Dec  7 12:04:28 2006 jofret
-** Last update Thu Sep  6 17:09:51 2007 bruno
-**
-** Copyright INRIA 2006
-*/
+ *  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ *  Copyright (C) 2006-2008 - INRIA - Bruno JOFRET
+ *
+ *  This file must be used under the terms of the CeCILL.
+ *  This source file is licensed as described in the file COPYING, which
+ *  you should have received as part of this distribution.  The terms
+ *  are also available at
+ *  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
 
 #include "exp10.h"
 
