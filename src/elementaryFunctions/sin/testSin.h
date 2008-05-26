c11d1fc83001d6aacc542d4b999b1f3cc5fda904
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2008-05-26 08:58:57 +0000

    License update

--- src/elementaryFunctions/sin/testSin.h
+++ src/elementaryFunctions/sin/testSin.h
@@ -1,14 +1,14 @@
 /*
-**  -*- C -*-
-**
-** testSin.h
-** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
-**
-** Started on  Mon Mar 26 17:21:21 2007 jofret
-** Last update Fri Apr 20 14:47:39 2007 jofret
-**
-** Copyright INRIA 2007
-*/
+ *  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ *  Copyright (C) 2007-2008 - INRIA - Bruno JOFRET
+ *
+ *  This file must be used under the terms of the CeCILL.
+ *  This source file is licensed as described in the file COPYING, which
+ *  you should have received as part of this distribution.  The terms
+ *  are also available at
+ *  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
 
 #ifndef _TESTSIN_H_
 #define _TESTSIN_H_
--- src/elementaryFunctions/sin/zsina.c
+++ src/elementaryFunctions/sin/zsina.c
@@ -1,14 +1,14 @@
 /*
-**  -*- C -*-
-**
-** zsina.c
-** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
-**
-** Started on  Fri Dec  8 14:02:04 2006 jofret
-** Last update Mon Oct 22 09:49:58 2007 bruno
-**
-** Copyright INRIA 2006
-*/
+ *  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ *  Copyright (C) 2006-2008 - INRIA - Bruno JOFRET
+ *
+ *  This file must be used under the terms of the CeCILL.
+ *  This source file is licensed as described in the file COPYING, which
+ *  you should have received as part of this distribution.  The terms
+ *  are also available at
+ *  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
 
 #include "sin.h"
 
