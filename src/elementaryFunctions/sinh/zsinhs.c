38ab2b240bfd0b5bd652ed6a71f5dd9cd0e03d9b
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2008-05-27 08:52:11 +0000

    License update

--- src/elementaryFunctions/sinh/zsinhs.c
+++ src/elementaryFunctions/sinh/zsinhs.c
@@ -1,14 +1,14 @@
 /*
-**  -*- C -*-
-**
-** zsinhs.c
-** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
-**
-** Started on  Fri Dec  8 12:06:35 2006 jofret
-** Last update Fri Feb 23 17:23:10 2007 jofret
-**
-** Copyright INRIA 2006
-*/
+ *  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ *  Copyright (C) 2006-2008 - INRIA - Bruno JOFRET
+ *
+ *  This file must be used under the terms of the CeCILL.
+ *  This source file is licensed as described in the file COPYING, which
+ *  you should have received as part of this distribution.  The terms
+ *  are also available at
+ *  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
 
 #include "sinh.h"
 #include "sin.h"
