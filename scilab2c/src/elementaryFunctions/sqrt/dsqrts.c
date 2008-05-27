2eb615dc01b042322b582c6f22583ae038e51ff5
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2008-05-27 13:10:46 +0000

    * Add Scilab square root algorithm. * Need some further testing

--- scilab2c/src/elementaryFunctions/sqrt/dsqrts.c
+++ scilab2c/src/elementaryFunctions/sqrt/dsqrts.c
@@ -1,14 +1,14 @@
 /*
-**  -*- C -*-
-**
-** dsqrts.c
-** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
-**
-** Started on  Fri Apr 20 10:59:31 2007 jofret
-** Last update Mon Apr 23 17:37:02 2007 jofret
-**
-** Copyright INRIA 2007
-*/
+ *  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ *  Copyright (C) 2007-2008 - INRIA - Bruno JOFRET
+ *
+ *  This file must be used under the terms of the CeCILL.
+ *  This source file is licensed as described in the file COPYING, which
+ *  you should have received as part of this distribution.  The terms
+ *  are also available at
+ *  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
 
 #include <math.h>
 #include "sqrt.h"
