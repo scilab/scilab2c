23e4c7f3e27cf5ef162646b4561f7d4b401ec22b
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2008-05-27 08:52:11 +0000

    License update

--- trunk/scilab2c/src/elementaryFunctions/sinh/dsinha.c
+++ trunk/scilab2c/src/elementaryFunctions/sinh/dsinha.c
@@ -1,14 +1,14 @@
 /*
-**  -*- C -*-
-**
-** dsinha.c
-** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
-**
-** Started on  Fri Dec  8 11:02:19 2006 jofret
-** Last update Mon Oct 22 09:50:41 2007 bruno
-**
-** Copyright INRIA 2006
-*/
+ *  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ *  Copyright (C) 2006-2008 - INRIA - Bruno JOFRET
+ *
+ *  This file must be used under the terms of the CeCILL.
+ *  This source file is licensed as described in the file COPYING, which
+ *  you should have received as part of this distribution.  The terms
+ *  are also available at
+ *  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
 
 #include "sinh.h"
 
