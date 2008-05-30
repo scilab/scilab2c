96d0f368713754bf4a53196fdae76d6adaef3586
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2008-05-30 09:11:49 +0000

    Adding statistics functions module for mean

---
+++ trunk/scilab2c/src/statisticsFunctions/includes/mean.h
@@ -0,0 +1,47 @@
+/*
+ *  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ *  Copyright (C) 2008-2008 - INRIA - Bruno JOFRET
+ *
+ *  This file must be used under the terms of the CeCILL.
+ *  This source file is licensed as described in the file COPYING, which
+ *  you should have received as part of this distribution.  The terms
+ *  are also available at
+ *  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
+
+#ifndef __MEAN_H__
+#define __MEAN_H__
+/*
+** \brief Mean of a scalar element, just returns it
+*/
+#define smeans(in)			in
+#define srowmeans(in)			in
+#define scolumnsmeans(in)		in
+#define smatlabmeans(in)		in
+
+/*
+** \brief Mean of a scalar element, just returns it
+*/
+#define dmeans(in)			in
+#define drowmeans(in)			in
+#define dcolumnsmeans(in)		in
+#define dmatlabmeans(in)		in
+
+/*
+** \brief Mean of a scalar element, just returns it
+*/
+#define cmeans(in)			in
+#define crowmeans(in)			in
+#define ccolumnsmeans(in)		in
+#define cmatlabmeans(in)		in
+
+/*
+** \brief Mean of a scalar element, just returns it
+*/
+#define zmeans(in)			in
+#define zrowmeans(in)			in
+#define zcolumnsmeans(in)		in
+#define zmatlabmeans(in)		in
+
+#endif /* !__MEAN_H__ */
