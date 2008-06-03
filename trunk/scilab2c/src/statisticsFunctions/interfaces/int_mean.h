614225b7964b6326e7a6d9dc486574e07496fd88
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2008-06-03 13:02:02 +0000

    * MEAN interface * SUM interface

---
+++ trunk/scilab2c/src/statisticsFunctions/interfaces/int_mean.h
@@ -0,0 +1,34 @@
+/*
+ * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ * Copyright (C) 2008-2008 - INRIA - Bruno JOFRET
+ * 
+ * This file must be used under the terms of the CeCILL.
+ * This source file is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at    
+ * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
+
+/* THIS IS AN AUTOMATICALLY GENERATED FILE : DO NOT EDIT BY HAND. */
+
+#ifndef __INT_MEAN_H__
+#define __INT_MEAN_H__
+
+#define s0means0(in)			smeans(in)
+
+#define d0meand0(in)			dmeans(in)
+
+#define c0meanc0(in)			cmeans(in)
+
+#define z0meanz0(in)			zmeans(in)
+
+#define s2means2(in,size,out)		smeana(in, size[0]*size[1], out)
+
+#define d2meand2(in,size,out)		dmeana(in, size[0]*size[1], out)
+
+#define c2meanc2(in,size,out)		cmeana(in, size[0]*size[1], out)
+
+#define z2meanz2(in,size,out)		zmeana(in, size[0]*size[1], out)
+
+#endif /* !__INT_MEAN_H__ */
---
+++ trunk/scilab2c/src/statisticsFunctions/interfaces/int_sum.h
@@ -0,0 +1,34 @@
+/*
+ * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ * Copyright (C) 2008-2008 - INRIA - Bruno JOFRET
+ * 
+ * This file must be used under the terms of the CeCILL.
+ * This source file is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at    
+ * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
+
+/* THIS IS AN AUTOMATICALLY GENERATED FILE : DO NOT EDIT BY HAND. */
+
+#ifndef __INT_SUM_H__
+#define __INT_SUM_H__
+
+#define s0sums0(in)			ssums(in)
+
+#define d0sumd0(in)			dsums(in)
+
+#define c0sumc0(in)			csums(in)
+
+#define z0sumz0(in)			zsums(in)
+
+#define s2sums2(in,size,out)		ssuma(in, size[0]*size[1], out)
+
+#define d2sumd2(in,size,out)		dsuma(in, size[0]*size[1], out)
+
+#define c2sumc2(in,size,out)		csuma(in, size[0]*size[1], out)
+
+#define z2sumz2(in,size,out)		zsuma(in, size[0]*size[1], out)
+
+#endif /* !__INT_SUM_H__ */
