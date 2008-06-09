95b00e5529296f61bbf9bf417df7c816207caa69
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2008-06-09 08:46:48 +0000

    - Adding zpythags - License update

--- src/auxiliaryFunctions/pythag/testPythag.h
+++ src/auxiliaryFunctions/pythag/testPythag.h
@@ -1,14 +1,15 @@
- /*
-**  -*- C -*-
-**
-** testPythag.h
-** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
-**
-** Started on  Mon Apr 23 16:26:14 2007 jofret
-** Last update Thu Apr 26 10:34:36 2007 jofret
-**
-** Copyright INRIA 2007
-*/
+/*
+ *  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ *  Copyright (C) 2007-2008 - INRIA - Bruno JOFRET
+ *
+ *  This file must be used under the terms of the CeCILL.
+ *  This source file is licensed as described in the file COPYING, which
+ *  you should have received as part of this distribution.  The terms
+ *  are also available at
+ *  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
+
 #ifndef _TESTPYTHAG_H_
 #define _TESTPYTHAG_H_
 
