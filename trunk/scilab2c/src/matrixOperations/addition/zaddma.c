fc0d4f432b38b4b141f41fe9b49fc45055943658
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2008-06-19 14:53:48 +0000

    Split Addition

--- trunk/scilab2c/src/matrixOperations/addition/zaddma.c
+++ trunk/scilab2c/src/matrixOperations/addition/zaddma.c
@@ -13,21 +13,21 @@
 #include "matrixAddition.h"
 
 /*
-** \brief Compute an addition element ways for floats.
+** \brief Compute an addition element ways for complex double precision.
 ** \param in1 : input array.
 ** \param size1 : size of in1 array.
 ** \param in2 : input arry.
 ** \param size2 : size of in2 array.
 ** \param out : array that contains the addition in1 + in2.
 */
-void	saddma(float *in1, int size1,
-	       float *in2, int size2,
-	       float *out)
+void	zaddma(doubleComplex *in1, int size1,
+	       doubleComplex *in2, int size2,
+	       doubleComplex *out)
 {
   int i = 0;
 
   for(i = 0 ; i < size1 && i < size2 ; ++i)
     {
-      out[i] = in1[i] + in2[i];
+      out[i] = zadds(in1[i], in2[i]);
     }
 }
