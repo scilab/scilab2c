0792a381efb86a52e5b202f1355b19a8ab0f2f6d
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2008-06-26 07:49:00 +0000

    automated generated headers

---
+++ trunk/scilab2c/src/elementaryFunctions/interfaces/int_atan.h
@@ -0,0 +1,50 @@
+/*
+ * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ * Copyright (C) 2008-2008 - INRIA - Bruno JOFRET
+ *
+ * This file must be used under the terms of the CeCILL.
+ * This source file is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at
+ * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
+
+/* THIS IS AN AUTOMATICALLY GENERATED FILE : DO NOT EDIT BY HAND. */
+
+#ifndef __INT_ATAN_H__
+#define __INT_ATAN_H__
+
+#define s0atans0(in)			satans(in)
+
+#define d0atand0(in)			datans(in)
+
+#define c0atanc0(in)			catans(in)
+
+#define z0atanz0(in)			zatans(in)
+
+#define s2atans2(in,size,out)		satana(in, size[0]*size[1], out)
+
+#define d2atand2(in,size,out)		datana(in, size[0]*size[1], out)
+
+#define c2atanc2(in,size,out)		catana(in, size[0]*size[1], out)
+
+#define z2atanz2(in,size,out)		zatana(in, size[0]*size[1], out)
+
+#define s0s0atans0(in1, in2)		satan2s(in1, in2)
+
+#define d0d0atand0(in1, in2)		datan2s(in1, in2)
+
+#define c0c0atanc0(in1, in2)		catan2s(in1, in2)
+
+#define z0z0atanz0(in1, in2)		zatan2s(in1, in2)
+
+#define s2s2atans2(in1, in2, out)	satan2a(in1, in2, out)
+
+#define d2d2atand2(in1, in2, out)	datan2a(in1, in2, out)
+
+#define c2c2atanc2(in1, in2, out)	catan2a(in1, in2, out)
+
+#define z2z2atanz2(in1, in2, out)	zatan2a(in1, in2, out)
+
+#endif /* !__INT_ATAN_H__ */
