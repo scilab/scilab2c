57693479d57abe0c08885827a6506dd4df16aab7
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2008-06-26 07:46:29 +0000

    Automated generated header

--- scilab2c/src/auxiliaryFunctions/interfaces/int_abs.h
+++ scilab2c/src/auxiliaryFunctions/interfaces/int_abs.h
@@ -1,54 +1,34 @@
 /*
-**  -*- C -*-
-**
-** int_abs.h
-** Made by  Raffaele Nutricato <raffaele.nutricato@tiscali.it>
-**
-**
-** Copyright POLIBA 2008
-*/
-/* 
-   1. Search for abs in INIT_FillSCI2LibCDirs.sci
-   2. Search for // --- Function List Class. --- in INIT_FillSCI2LibCDirs.sci
-   3. You will find 
-      PrintStringInfo('s0'+ArgSeparator+'s0',ClassFileName,'file','y');
-      PrintStringInfo('d0'+ArgSeparator+'d0',ClassFileName,'file','y');
-      PrintStringInfo('c0'+ArgSeparator+'s0',ClassFileName,'file','y');
-      PrintStringInfo('z0'+ArgSeparator+'d0',ClassFileName,'file','y');
-      PrintStringInfo('s2'+ArgSeparator+'s2',ClassFileName,'file','y');
-      PrintStringInfo('d2'+ArgSeparator+'d2',ClassFileName,'file','y');
-      PrintStringInfo('c2'+ArgSeparator+'s2',ClassFileName,'file','y');
-      PrintStringInfo('z2'+ArgSeparator+'d2',ClassFileName,'file','y');
-   4. These are all the functions to be implemented.
-   5. According to functions available in src you have to generate the mapping between
-      functions in step3 and functions in src.
-*/
+ * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ * Copyright (C) 2008-2008 - INRIA - Bruno JOFRET
+ * 
+ * This file must be used under the terms of the CeCILL.
+ * This source file is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at    
+ * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
+
+/* THIS IS AN AUTOMATICALLY GENERATED FILE : DO NOT EDIT BY HAND. */
 
 #ifndef __INT_ABS_H__
 #define __INT_ABS_H__
 
-#define s0abss0(in) \
-sabss(in);
+#define s0abss0(in)			sabss(in)
 
-#define d0absd0(in) \
-dabss(in);
+#define d0absd0(in)			dabss(in)
 
-#define c0abss0(in) \
-cabss(in);
+#define c0absc0(in)			cabss(in)
 
-#define z0absd0(in) \
-zabss(in);
+#define z0absz0(in)			zabss(in)
 
-#define s2abss2(inptr,insizeptr,outptr) \
-sabsa(inptr, insizeptr[0]*insizeptr[1], outptr);
+#define s2abss2(in,size,out)		sabsa(in, size[0]*size[1], out)
 
-#define d2absd2(inptr,insizeptr,outptr) \
-dabsa(inptr, insizeptr[0]*insizeptr[1], outptr);
+#define d2absd2(in,size,out)		dabsa(in, size[0]*size[1], out)
 
-#define c2abss2(inptr,insizeptr,outptr) \
-cabsa(inptr, insizeptr[0]*insizeptr[1], outptr);
+#define c2absc2(in,size,out)		cabsa(in, size[0]*size[1], out)
 
-#define z2absd2(inptr,insizeptr,outptr) \
-zabsa(inptr, insizeptr[0]*insizeptr[1], outptr);
+#define z2absz2(in,size,out)		zabsa(in, size[0]*size[1], out)
 
 #endif /* !__INT_ABS_H__ */
