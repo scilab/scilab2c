3ab6ffeb6f9e2eab2d61b764b12383c087949972
simon <simon@34710fda-3622-0410-992e-dbbbfc6a01d4>
2008-07-11 14:22:34 +0000

    added tranpose operation

---
+++ scilab2c/src/matrixOperations/transpose/ctransposea.c
@@ -0,0 +1,28 @@
+/*
+ *  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ *  Copyright (C) 2008-2008 - INRIA - Allan SIMON
+ *
+ *  This file must be used under the terms of the CeCILL.
+ *  This source file is licensed as described in the file COPYING, which
+ *  you should have received as part of this distribution.  The terms
+ *  are also available at
+ *  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
+
+#include "matrixTranspose.h"
+
+void ctransposea ( floatComplex* in , int lines1 , int column1, floatComplex* out ){
+
+	
+	int newCoord = 0 ;
+	int index    = 0 ;
+	
+	for(index = 0 ; index <  lines1 * column1 ; index++)
+	{
+		 newCoord	= index % column1 * lines1 + ( index / column1);
+		/* Hermitian transpose*/
+		out[newCoord]	=  FloatComplex (  creals( in[index]) , -cimags ( in[index]));
+	}
+
+}
