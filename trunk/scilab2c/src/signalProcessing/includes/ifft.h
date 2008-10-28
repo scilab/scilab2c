a34fcdd5139e052fec43442e312f029a66965e5a
simon <simon@34710fda-3622-0410-992e-dbbbfc6a01d4>
2008-10-28 14:36:18 +0000

    added comment

--- trunk/scilab2c/src/signalProcessing/includes/ifft.h
+++ trunk/scilab2c/src/signalProcessing/includes/ifft.h
@@ -20,7 +20,24 @@
 #include "lapack.h"
 #include "fft_internal.h"
 
+/*
+** compute the inverse fast fourier transform of a vector
+** param in  : the input matrix in complex double precision
+** param rows: number of rows of the input matrix
+** param cols: number of cols of the input matrix
+** param out : the transformed matrix in complex double precision
+*/
+
+
 void zifftma ( doubleComplex* in , int rows, int cols, doubleComplex* out);
+/*
+** compute the inverse fast fourier transform of a vector
+** param in  : the input matrix in complex float precision
+** param rows: number of rows of the input matrix
+** param cols: number of cols of the input matrix
+** param out : the transformed matrix in complex float precision
+*/
+
 void cifftma ( floatComplex* in , int rows, int cols, floatComplex* out);
 
 
