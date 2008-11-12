c6dde987d9504aff512852e23a2cea106fd7801c
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2008-11-12 08:37:29 +0000

    Header update

--- trunk/scilab2c/src/elementaryFunctions/tanh/ctanhs.c
+++ trunk/scilab2c/src/elementaryFunctions/tanh/ctanhs.c
@@ -1,14 +1,14 @@
 /*
-**  -*- C -*-
-**
-** ctanhs.c
-** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
-**
-** Started on  Thu Dec  7 12:04:28 2006 jofret
-** Last update Fri Feb 23 17:31:52 2007 jofret
-**
-** Copyright INRIA 2006
-*/
+ *  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ *  Copyright (C) 2006-2008 - INRIA - Bruno JOFRET
+ *
+ *  This file must be used under the terms of the CeCILL.
+ *  This source file is licensed as described in the file COPYING, which
+ *  you should have received as part of this distribution.  The terms
+ *  are also available at
+ *  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
 
 #include "tanh.h"
 #include "tan.h"
