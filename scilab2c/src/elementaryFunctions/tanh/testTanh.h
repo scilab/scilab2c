f1570dcab24707bc6c2e9941258ff428bdd9fd0e
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2008-11-12 08:37:29 +0000

    Header update

--- scilab2c/src/elementaryFunctions/tanh/testTanh.h
+++ scilab2c/src/elementaryFunctions/tanh/testTanh.h
@@ -1,14 +1,14 @@
 /*
-**  -*- C -*-
-**
-** testTanh.h
-** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
-**
-** Started on  Fri Mar 30 14:04:02 2007 jofret
-** Last update Fri Mar 30 14:12:06 2007 jofret
-**
-** Copyright INRIA 2007
-*/
+ *  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ *  Copyright (C) 2007-2008 - INRIA - Bruno JOFRET
+ *
+ *  This file must be used under the terms of the CeCILL.
+ *  This source file is licensed as described in the file COPYING, which
+ *  you should have received as part of this distribution.  The terms
+ *  are also available at
+ *  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
 
 #ifndef _TESTTANH_H_
 #define _TESTTANH_H_
