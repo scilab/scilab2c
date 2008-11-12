a0fc0932133195869cfeff93afb8e0eb3d14c1be
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2008-11-12 14:33:10 +0000

    First generation, need improvement

---
+++ scilab2c/src/auxiliaryFunctions/interfaces/int_find.h
@@ -0,0 +1,34 @@
+/*
+ * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ * Copyright (C) 2008-2008 - INRIA - Bruno JOFRET
+ * 
+ * This file must be used under the terms of the CeCILL.
+ * This source file is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at    
+ * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
+
+/* THIS IS AN AUTOMATICALLY GENERATED FILE : DO NOT EDIT BY HAND. */
+
+#ifndef __INT_FIND_H__
+#define __INT_FIND_H__
+
+#define s0finds0(in)			sfinds(in)
+
+#define d0findd0(in)			dfinds(in)
+
+#define c0findc0(in)			cfinds(in)
+
+#define z0findz0(in)			zfinds(in)
+
+#define s2finds2(in,size,out)		sfinda(in, size[0]*size[1], out)
+
+#define d2findd2(in,size,out)		dfinda(in, size[0]*size[1], out)
+
+#define c2findc2(in,size,out)		cfinda(in, size[0]*size[1], out)
+
+#define z2findz2(in,size,out)		zfinda(in, size[0]*size[1], out)
+
+#endif /* !__INT_FIND_H__ */
