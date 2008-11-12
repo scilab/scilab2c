1cd50735d21fa6a87eb8d33eca2ad0dcb1edd754
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2008-11-12 08:37:29 +0000

    Header update

--- src/elementaryFunctions/asinh/testAsinh.h
+++ src/elementaryFunctions/asinh/testAsinh.h
@@ -1,14 +1,14 @@
 /*
-**  -*- C -*-
-**
-** testAsinh.h
-** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
-**
-** Started on  Mon Mar 26 16:33:11 2007 jofret
-** Last update Thu Sep  6 11:20:20 2007 bruno
-**
-** Copyright INRIA 2007
-*/
+ *  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ *  Copyright (C) 2007-2008 - INRIA - Bruno JOFRET
+ *
+ *  This file must be used under the terms of the CeCILL.
+ *  This source file is licensed as described in the file COPYING, which
+ *  you should have received as part of this distribution.  The terms
+ *  are also available at
+ *  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
 
 #ifndef _TESTASINH_H_
 #define _TESTASINH_H_
