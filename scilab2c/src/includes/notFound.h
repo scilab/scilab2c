eab177e4ef917964406d6ae8e7ca4819877101e6
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2008-11-12 08:38:38 +0000

    Header update

--- scilab2c/src/includes/notFound.h
+++ scilab2c/src/includes/notFound.h
@@ -1,14 +1,14 @@
 /*
-**  -*- C -*-
-**
-** notFound.h
-** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
-**
-** Started on  Thu Feb  8 10:12:17 2007 jofret
-** Last update Tue Feb 13 17:16:47 2007 jofret
-**
-** Copyright INRIA 2007
-*/
+ *  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ *  Copyright (C) 2007-2008 - INRIA - Bruno JOFRET
+ *
+ *  This file must be used under the terms of the CeCILL.
+ *  This source file is licensed as described in the file COPYING, which
+ *  you should have received as part of this distribution.  The terms
+ *  are also available at
+ *  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
 
 #ifndef __NOT_FOUND_H__
 #define __NOT_FOUND_H__
