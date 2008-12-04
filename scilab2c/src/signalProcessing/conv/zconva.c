eb2eef4e4edf257eedc331e202f9e511092584d4
torset <torset@34710fda-3622-0410-992e-dbbbfc6a01d4>
2008-12-04 10:56:09 +0000

    Add conv repertory and files

---
+++ scilab2c/src/signalProcessing/conv/zconva.c
@@ -0,0 +1,34 @@
+#include "conv.h"
+
+void zconva(doubleComplex *in1, int size1, doubleComplex *in2,int size2, doubleComplex *out){
+	int m1,i;
+	doubleComplex *in1b, *in2b, *result;	
+
+
+	m1=(int)floor(log(size1+size2-1)/log(2)+1);
+	m1=(int)pow(2,m1);
+
+	in1b=malloc(2*(unsigned int)m1*sizeof(double));
+	for(i=0;i<m1;i++){
+		if (i<size1) in1b[i]=in1[i];
+		else in1b[i]=DoubleComplex(0,0);
+	}
+	
+	in2b=malloc(2*(unsigned int)m1*sizeof(double));
+	for(i=0;i<m1;i++){
+		if (i<size2) in2b[i]=in2[i];
+		else in2b[i]=DoubleComplex(0,0);
+	}
+
+	
+	zfftma(in1b,m1,1,in1b);
+	zfftma(in2b,m1,1,in2b);
+
+	result=malloc(2*(unsigned int)m1*sizeof(double));
+	zmula(in1b,in2b,m1,result);
+	zifftma(result,m1,1,result);	
+	
+	for (i=0;i<size1+size2-1;i++){
+		out[i]=result[i];
+	}
+}
