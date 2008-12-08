5e59f4226dd4bdea74de559e81cb2ab89100d292
torset <torset@34710fda-3622-0410-992e-dbbbfc6a01d4>
2008-12-08 10:50:16 +0000

    Add L-U decomposition of square matrix using Cholesky algorithm(chol) testDoubleChol failed

---
+++ trunk/scilab2c/src/matrixOperations/chol/dchola.c
@@ -0,0 +1,34 @@
+/*
+ *  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ *  Copyright (C) 2008 - INRIA - Arnaud TORSET
+ *
+ *  This file must be used under the terms of the CeCILL.
+ *  This source file is licensed as described in the file COPYING, which
+ *  you should have received as part of this distribution.  The terms
+ *  are also available at
+ *  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
+
+
+
+#include "chol.h"
+#include <malloc.h>
+#include <stdio.h>
+
+void dchola(double * in, int size, double *U){
+	/* param in : input matrix (square matrix)
+	   param size : number of rows or columns
+	   param U : output upper triangular matrix
+	*/
+	double* tmp;
+	int i;
+	tmp=malloc((unsigned int)size*sizeof(double));
+	for (i=0;i<size*size;i++) tmp[i]=in[i];
+	printf("copy\n");
+	C2F(dpotrf)('U',size,in,size,0);
+	printf("lapack\n");
+	for (i=0;i<size*size;i++) U[i]=in[i];
+	printf("copy2\n");
+	
+}
