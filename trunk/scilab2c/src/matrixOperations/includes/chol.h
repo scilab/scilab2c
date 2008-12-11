ea6bf1176c98276fcdfb42e87cf3239dfbce0b28
torset <torset@34710fda-3622-0410-992e-dbbbfc6a01d4>
2008-12-11 15:11:01 +0000

    Modify includes in chol.h

--- trunk/scilab2c/src/matrixOperations/includes/chol.h
+++ trunk/scilab2c/src/matrixOperations/includes/chol.h
@@ -14,26 +14,20 @@
 #ifndef __CHOL_H__
 #define __CHOL_H__
  
-#include "lapack.h"
 #include "doubleComplex.h"
 #include "floatComplex.h"
  
-	/* 
-	chol : L-U decomposition of square matrix using Cholesky algorithm
-	   param in : input matrix (square matrix)
-	   param size : number of rows or columns
-	   param out : output matrix
-	*/ 
+
 	
 double dchols (double in);
 void dchola (double *in, int size, double *out);
- /* 
-void schols (float in,  float out1);
+ 
+float schols (float in);
 void schola (float *in, int size, float *out);
  
 void zchola (doubleComplex *in, int size, doubleComplex *out); 
  
-void cchola (floatComplex *in, int size, floatComplex *out); */
+void cchola (floatComplex *in, int size, floatComplex *out); 
 
 
 #endif /* __CHOL_H__ */
