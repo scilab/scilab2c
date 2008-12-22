b113fba27af6dcd69f8688bfb5807bd89b871ab0
torset <torset@34710fda-3622-0410-992e-dbbbfc6a01d4>
2008-12-22 09:07:41 +0000

    Little changes made

--- trunk/scilab2c/src/signalProcessing/conv/dconva.c
+++ trunk/scilab2c/src/signalProcessing/conv/dconva.c
@@ -1,3 +1,16 @@
+
+/*
+ *  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ *  Copyright (C) 2008 - INRIA - Arnaud TORSET
+ *
+ *  This file must be used under the terms of the CeCILL.
+ *  This source file is licensed as described in the file COPYING, which
+ *  you should have received as part of this distribution.  The terms
+ *  are also available at
+ *  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
+
 #include "conv.h"
 
 void dconva(double *in1, int size1, double *in2,int size2, double *out){
@@ -18,6 +31,9 @@ void dconva(double *in1, int size1, double *in2,int size2, double *out){
 	zreala(result,size1+size2-1,out);
 
 	free(result);
+	free(in2Cpx);
+	free(in1Cpx);
+
 }
 
 
