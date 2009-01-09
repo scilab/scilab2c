ae902b67ffb303d65ce7871fc28d9c2ddd7aa435
torset <torset@34710fda-3622-0410-992e-dbbbfc6a01d4>
2009-01-09 15:48:43 +0000

    Updates Interfaces

---
+++ scilab2c/src/auxiliaryFunctions/interfaces/int_isempty.h
@@ -0,0 +1,34 @@
+/*
+ * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ * Copyright (C) 2008-2008 - INRIA - Bruno JOFRET
+ * 
+ * This file must be used under the terms of the CeCILL.
+ * This source file is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at    
+ * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
+
+/* THIS IS AN AUTOMATICALLY GENERATED FILE : DO NOT EDIT BY HAND. */
+
+#ifndef __INT_ISEMPTY_H__
+#define __INT_ISEMPTY_H__
+
+#define s0isemptys0(in)			sisemptys(in)
+
+#define d0isemptyd0(in)			disemptys(in)
+
+#define c0isemptyc0(in)			cisemptys(in)
+
+#define z0isemptyz0(in)			zisemptys(in)
+
+#define s2isemptys0(in,size,out)		sisemptya(in, size[0]*size[1])
+
+#define d2isemptyd0(in,size,out)		disemptya(in, size[0]*size[1])
+
+#define c2isemptyc0(in,size,out)		cisemptya(in, size[0]*size[1])
+
+#define z2isemptyz0(in,size,out)		zisemptya(in, size[0]*size[1])
+
+#endif /* !__INT_ISEMPTY_H__ */
--- scilab2c/src/auxiliaryFunctions/interfaces/int_isnan.h
+++ scilab2c/src/auxiliaryFunctions/interfaces/int_isnan.h
@@ -12,25 +12,23 @@
 
 /* THIS IS AN AUTOMATICALLY GENERATED FILE : DO NOT EDIT BY HAND. */
 
-#ifndef __INT_CONJ_H__
-#define __INT_CONJ_H__
+#ifndef __INT_ISNAN_H__
+#define __INT_ISNAN_H__
 
-#define copy(in, size, out)		for (int i=0; i<size;i++) out[i]=in[i];
+#define s0isnans0(in)			sisnans(in)
 
-#define s0conjs0(in)			in
+#define d0isnand0(in)			disnans(in)
 
-#define d0conjd0(in)			in
+#define c0isnanc0(in)			cisnans(in)
 
-#define c0conjc0(in)			cconjs(in)
+#define z0isnanz0(in)			zisnans(in)
 
-#define z0conjz0(in)			zconjs(in)
+#define s2isnans2(in,size,out)		sisnana(in, size[0]*size[1], out)
 
-#define s2conjs2(in,size,out)		copy(in,size,out)
+#define d2isnand2(in,size,out)		disnana(in, size[0]*size[1], out)
 
-#define d2conjd2(in,size,out)		copy(in,size,out)
+#define c2isnanc2(in,size,out)		cisnana(in, size[0]*size[1], out)
 
-#define c2conjc2(in,size,out)		cconja(in, size[0]*size[1], out)
+#define z2isnanz2(in,size,out)		zisnana(in, size[0]*size[1], out)
 
-#define z2conjz2(in,size,out)		zconja(in, size[0]*size[1], out)
-
-#endif /* !__INT_CONJ_H__ */
+#endif /* !__INT_ISNAN_H__ */
