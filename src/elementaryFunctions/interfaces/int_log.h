d0e7c64c1dfd1e2f2243355650436c5ebde0fdbf
torset <torset@34710fda-3622-0410-992e-dbbbfc6a01d4>
2009-01-09 15:11:14 +0000

    Updates Interfaces

--- src/elementaryFunctions/interfaces/int_log.h
+++ src/elementaryFunctions/interfaces/int_log.h
@@ -17,7 +17,11 @@
 
 #define s0logs0(in)			slogs(in)
 
+#define s0logc0(in)			clogs(FloatComplex(in,0))
+
 #define d0logd0(in)			dlogs(in)
+
+#define d0logz0(in)			zlogs(DoubleComplex(in,0))
 
 #define c0logc0(in)			clogs(in)
 
@@ -25,7 +29,11 @@
 
 #define s2logs2(in,size,out)		sloga(in, size[0]*size[1], out)
 
+#define s2logc2(in,size,out)		cloga(FloatComplexMatrix(in,0), size[0]*size[1], out)
+
 #define d2logd2(in,size,out)		dloga(in, size[0]*size[1], out)
+
+#define d2logz2(in,size,out)		zloga(DoubleComplexMatrix(in,0), size[0]*size[1], out)
 
 #define c2logc2(in,size,out)		cloga(in, size[0]*size[1], out)
 
