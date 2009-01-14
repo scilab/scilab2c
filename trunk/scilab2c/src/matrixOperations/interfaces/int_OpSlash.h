ec17e085a42a5ef873af70e3b5b36ff7f40dfe80
torset <torset@34710fda-3622-0410-992e-dbbbfc6a01d4>
2009-01-14 09:36:53 +0000

    add the interface for matrixOperations (contains only operations with matrices on both input)

---
+++ trunk/scilab2c/src/matrixOperations/interfaces/int_OpSlash.h
@@ -0,0 +1,34 @@
+/*
+ * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ * Copyright (C) 2008-2008 - INRIA - Bruno JOFRET
+ * 
+ * This file must be used under the terms of the CeCILL.
+ * This source file is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at    
+ * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
+
+/* THIS IS AN AUTOMATICALLY GENERATED FILE : DO NOT EDIT BY HAND. */
+
+#ifndef __INT_OPSLASH_H__
+#define __INT_OPSLASH_H__
+
+#define s0rdivms0(in)			srdivms(in)
+
+#define d0rdivmd0(in)			drdivms(in)
+
+#define c0rdivmc0(in)			crdivms(in)
+
+#define z0rdivmz0(in)			zrdivms(in)
+
+#define s2rdivms2(in,size,out)		srdivma(in, size[0]*size[1], out)
+
+#define d2rdivmd2(in,size,out)		drdivma(in, size[0]*size[1], out)
+
+#define c2rdivmc2(in,size,out)		crdivma(in, size[0]*size[1], out)
+
+#define z2rdivmz2(in,size,out)		zrdivma(in, size[0]*size[1], out)
+
+#endif /* !__INT_OPSLASH_H__ */
