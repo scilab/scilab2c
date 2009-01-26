3126800c90de0a1a3162b01b3b254518607b048d
torset <torset@34710fda-3622-0410-992e-dbbbfc6a01d4>
2009-01-26 12:34:09 +0000

    Update

--- scilab2c/src/Scilab2C/Scilab2C/CFiles/sci2cincludes/sci2clib.h
+++ scilab2c/src/Scilab2C/Scilab2C/CFiles/sci2cincludes/sci2clib.h
@@ -50,54 +50,76 @@
 #include "log10.h"
 #include "int_log10.h"
 #include "pythag.h"
+
+/* LIB */
+/* interefacing lapack */
+#include "lapack.h"
+/* interefacing blas */
+#include "blas.h"
+
+/* AUXILIARY FUNCTIONS */
+
 /* interfacing find */
 #include "find.h"
 #include "int_find.h"
-/* interfacing cat */
-#include "cat.h"
-#include "int_OpRc.h"
-#include "int_OpCc.h"
 /* interfacing disp */
 #include "disp.h"
-#include "int_disp.h"/* interfacing multiplication */
-#include "multiplication.h"
-#include "matrixMultiplication.h"
-#include "int_OpStar.h"
-#include "int_OpDotStar.h"
+#include "int_disp.h"
+/* interfacing conj */
+#include "conj.h"
+#include "int_conj.h"
+
+/* ELEMENTARY FUNCTIONS */
+
+/* interfacing acos */
+#include "acos.h"
+#include "int_acos.h"
+/* interfacing atan */
+#include "atan.h"
+#include "int_atan.h"
+/* interfacing lnp1m1 */
+#include "lnp1m1.h"
+/* interfacing pow */
+#include "pow.h"
+#include "int_pow.h"
+
+
+
+/* OPERATIONS */
 /* interfacing addition */
 #include "addition.h"
 #include "int_OpPlus.h"/* interfacing subtraction */
 #include "subtraction.h"
-#include "int_OpMinus.h"
-/* interfacing fill */
-#include "fill.h"/* interfacing ones */
-#include "ones.h"/* interfacing division */
+#include "int_OpMinus.h"/* interfacing multiplication */
+#include "multiplication.h"
+#include "matrixMultiplication.h"
+#include "int_OpStar.h"
+#include "int_OpDotStar.h"
+/* interfacing division */
 #include "division.h"
 #include "int_OpSlash.h"
 #include "int_OpDotSlash.h"
 #include "int_OpBackSlash.h"
 #include "int_OpDotBackSlash.h"
-/* interfacing conj */
-#include "conj.h"
-#include "int_conj.h"
-/* interfacing acos */
-#include "acos.h"
-#include "int_acos.h"
-/* interefacing lapack */
-#include "lapack.h"
-/* interfacing atan */
-#include "atan.h"
-#include "int_atan.h"
-/* interefacing lnp1m1 */
-#include "lnp1m1.h"
-/* interefacing blas */
-#include "blas.h"
+
+
+
+/* MATRIX OPERTAIONS */
+/* interfacing cat */
+#include "cat.h"
+#include "int_OpRc.h"
+#include "int_OpCc.h"
+/* interfacing fill */
+#include "fill.h"/* interfacing ones */
+#include "ones.h"
 /* interefacing eye */
 #include "eye.h"
 #include "int_eye.h"
-/* interefacing trace */
+/* interfacing trace */
 #include "matrixTrace.h"
 #include "int_trace.h"
+/* interfacing tranpose */
+#include "matrixTranspose.h"
 
 /* STATISTICS FUNCTIONS */
 
