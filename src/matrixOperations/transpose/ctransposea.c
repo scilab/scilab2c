389138519aba9337322cb1ac753b01584d50357c
torset <torset@34710fda-3622-0410-992e-dbbbfc6a01d4>
2009-01-27 10:14:55 +0000

    Simplify transpose

--- src/matrixOperations/transpose/ctransposea.c
+++ src/matrixOperations/transpose/ctransposea.c
@@ -12,17 +12,16 @@
 
 #include "matrixTranspose.h"
 
-void ctransposea ( floatComplex* in , int lines1 , int column1, floatComplex* out ){
-
+void ctransposea ( floatComplex* in , int lines , int columns, floatComplex* out ){
 	
-	int newCoord = 0 ;
-	int index    = 0 ;
+	int i    = 0 ;
+	int j    = 0 ;
 	
-	for(index = 0 ; index <  lines1 * column1 ; index++)
+	for(i = 0 ; i <  lines ; i++)
 	{
-		 newCoord	= index % column1 * lines1 + ( index / column1);
+		for(j = 0 ; j <  columns ; j++)
 
-		out[newCoord]	=  FloatComplex (  creals( in[index]) , cimags ( in[index]));
+		out[j+i*columns]	= in[i+j*lines];
 	}
-
+	
 }
