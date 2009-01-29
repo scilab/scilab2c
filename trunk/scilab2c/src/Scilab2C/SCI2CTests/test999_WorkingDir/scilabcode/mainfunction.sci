faef59fa67cd3d3820bafc7ce61a60cb3d9a36ea
torset <torset@34710fda-3622-0410-992e-dbbbfc6a01d4>
2009-01-29 08:08:21 +0000

    Files for multiplication test which failed

--- trunk/scilab2c/src/Scilab2C/SCI2CTests/test999_WorkingDir/scilabcode/mainfunction.sci
+++ trunk/scilab2c/src/Scilab2C/SCI2CTests/test999_WorkingDir/scilabcode/mainfunction.sci
@@ -1,20 +1,56 @@
-//SCI2C: DEFAULT_PRECISION= FLOAT
+//SCI2C: DEFAULT_PRECISION= DOUBLE
 function mainfunction()
 
-a=[1 2 3 4; 5 6 7 8; 9 1 2 3; 4 5 6 7; 8 9 1 2]
+//test multiplication
 
-disp(acosh(a))
+//d0d0
+disp (5*3)
+//d0z0
+disp (1*%i)
+//z0d0
+disp (%i*1)
+//z0z0
+disp (%i*3*%i)
+
+a=[1 2 3; 4 5 6]
+c=[4 2 3 ;8 8 4; 3 4 5]
+b=[1+%i,2+4*%i 3+1*%i; 4 5 6+2*%i]
+d=[4 2 3 ;8 8-1*%i 4; 3+4*%i 4 5]
+e=[4 2 ;3 8 ;8-1*%i 4; 3+4*%i 4]
+
+//d2d2
+disp(a*c)
+disp(a.*a)
+//d2z2
+disp(a*d)
+disp(a.*b)
+//z2d2
+disp(e*a)
+disp(b.*a)
+//z2z2
+disp(b*d)
+disp(b.*b)
 
 
-b=acosh(.10)
-disp(b)
 
-c=a/10
-disp(acosh(c));
 
-d=acosh(2+3*%i)
-disp(d)
+//d2d0
+disp(a*1)
+//d0d2
+disp(1*a)
+//d2z0
+disp(a*%i)
+//z0d2
+disp(%i*a)
 
-disp(acosh(c+%i));
+
+//z2d0
+disp(b*1)
+//d0z2
+disp(1*b)
+//z2z0
+disp(b*%i)
+//z0z2
+disp(%i*b)
 
 endfunction
--- trunk/scilab2c/src/operations/interface/int_OpDotStar.h
+++ trunk/scilab2c/src/operations/interface/int_OpDotStar.h
@@ -36,48 +36,48 @@
 
 /* Scalar .* Matrix */
 
-#define s0s2OpDotStars2(in1,in2,size,out)			{int i;\
-									for(int i=0;i<size[0]*size[1];i++) out[i]= smuls(in1,in2[i]);}
+#define s0s2OpDotStars2(in1,in2,size,out)			{int i=0;\
+									for(i=0;i<size[0]*size[1];i++) out[i]= smuls(in1,in2[i]);}
 
-#define d0d2OpDotStard2(in1,in2,size,out)			{int i;\
-									for(int i=0;i<size[0]*size[1];i++) out[i]= dmuls(in1,in2[i]);}
+#define d0d2OpDotStard2(in1,in2,size,out)			{int i=0;\
+									for(i=0;i<size[0]*size[1];i++) out[i]= dmuls(in1,in2[i]);}
 
-#define c0c2OpDotStarc2(in1,in2,size,out)			{int i;\
-									for(int i=0;i<size[0]*size[1];i++) out[i]= cmuls(in1,in2[i]);}
+#define c0c2OpDotStarc2(in1,in2,size,out)			{int i=0;\
+									for(i=0;i<size[0]*size[1];i++) out[i]= cmuls(in1,in2[i]);}
 
-#define z0z2OpDotStarz2(in1,in2,size,out)			{int i;\
-									for(int i=0;i<size[0]*size[1];i++) out[i]= zmuls(in1,in2[i]);}
+#define z0z2OpDotStarz2(in1,in2,size,out)			{int i=0;\
+									for(i=0;i<size[0]*size[1];i++) out[i]= zmuls(in1,in2[i]);}
 
 #define s0c2OpDotStarc2(in1,in2,size,out)			c0c2OpDotStarc2(FloatComplex(in1,0),in2,size,out)
 
 #define d0z2OpDotStarz2(in1,in2,size,out)			z0z2OpDotStarz2(DoubleComplex(in1,0),in2,size,out)
 
-#define c0s2OpDotStarc2(in1,in2,size,out)			{int i;\
-									for(int i=0;i<size[0]*size[1];i++) out[i]= cmuls(in1,FloatComplex(in2[i],0));}
+#define c0s2OpDotStarc2(in1,in2,size,out)			{int i=0;\
+									for(i=0;i<size[0]*size[1];i++) out[i]= cmuls(in1,FloatComplex(in2[i],0));}
 
-#define z0d2OpDotStarz2(in1,in2,size,out)			{int i;\
-									for(int i=0;i<size[0]*size[1];i++) out[i]= zmuls(in1,DoubleComplex(in2[i],0));}
+#define z0d2OpDotStarz2(in1,in2,size,out)			{int i=0;\
+									for(i=0;i<size[0]*size[1];i++) out[i]= zmuls(in1,DoubleComplex(in2[i],0));}
 
 
 /* Matrix .* Scalar */
 
-#define s2s0OpDotStars2(in1,size,in2,out)			{int i;\
-									for(int i=0;i<size[0]*size[1];i++) out[i]= smuls(in1[i],in2);}
+#define s2s0OpDotStars2(in1,size,in2,out)			{int i=0;\
+									for(i=0;i<size[0]*size[1];i++) out[i]= smuls(in1[i],in2);}
 
-#define d2d0OpDotStard2(in1,size,in2,out)			{int i;\
-									for(int i=0;i<size[0]*size[1];i++) out[i]= dmuls(in1[i],in2);}
+#define d2d0OpDotStard2(in1,size,in2,out)			{int i=0;\
+									for(i=0;i<size[0]*size[1];i++) out[i]= dmuls(in1[i],in2);}
 
-#define c2c0OpDotStarc2(in1,size,in2,out)			{int i;\
-									for(int i=0;i<size[0]*size[1];i++) out[i]= cmuls(in1[i],in2);}
+#define c2c0OpDotStarc2(in1,size,in2,out)			{int i=0;\
+									for(i=0;i<size[0]*size[1];i++) out[i]= cmuls(in1[i],in2);}
 
-#define z2z0OpDotStarz2(in1,size,in2,out)			{int i;\
-									for(int i=0;i<size[0]*size[1];i++) out[i]= zmuls(in1[i],in2);}
+#define z2z0OpDotStarz2(in1,size,in2,out)			{int i=0;\
+									for(i=0;i<size[0]*size[1];i++) out[i]= zmuls(in1[i],in2);}
 
-#define s2c0OpDotStarc2(in1,size,in2,out)			{int i;\
-									for(int i=0;i<size[0]*size[1];i++) out[i]= cmuls(FloatComplex(in1[i],0),in2);}
+#define s2c0OpDotStarc2(in1,size,in2,out)			{int i=0;\
+									for(i=0;i<size[0]*size[1];i++) out[i]= cmuls(FloatComplex(in1[i],0),in2);}
 
-#define d2z0OpDotStarz2(in1,size,in2,out)			{int i;\
-									for(int i=0;i<size[0]*size[1];i++) out[i]= zmuls(DoubleComplex(in1[i],0),in2);}
+#define d2z0OpDotStarz2(in1,size,in2,out)			{int i=0;\
+									for(i=0;i<size[0]*size[1];i++) out[i]= zmuls(DoubleComplex(in1[i],0),in2);}
 
 #define c2s0OpDotStarc2(in1,size,in2,out)			c2c0OpDotStarc2(in1,size,FloatComplex(in2,0),out)
 
@@ -94,18 +94,18 @@
 #define z2z2OpDotStarz2(in1,size1,in2,size2,out)		zmula(in1,in2,size1[0]*size2[1],out)
 
 
-#define c2s2OpDotStarc2(in1,size1,in2,size2,out)		{int i;\
-										for(int i=0;i<size[0]*size[1];i++) out[i]= cmuls(in1[i],FloatComplex(in2[i],0),out)}
+#define c2s2OpDotStarc2(in1,size1,in2,size2,out)		{int i=0;\
+										for(i=0;i<size1[0]*size2[1];i++) out[i]= cmuls(in1[i],FloatComplex(in2[i],0));}
 
-#define s2c2OpDotStarc2(in1,size1,in2,size2,out)		{int i;\
-										for(int i=0;i<size[0]*size[1];i++) out[i]= cmuls(FloatComplex(in1[i],0),in2[i],out)}
+#define s2c2OpDotStarc2(in1,size1,in2,size2,out)		{int i=0;\
+										for(i=0;i<size1[0]*size2[1];i++) out[i]= cmuls(FloatComplex(in1[i],0),in2[i]);}
 
 
-#define z2d2OpDotStarc2(in1,size1,in2,size2,out)		{int i;\
-										for(int i=0;i<size[0]*size[1];i++) out[i]= zmuls(in1[i],DoubleComplex(in2[i],0),out)}
+#define z2d2OpDotStarz2(in1,size1,in2,size2,out)		{int i=0;\
+										for(i=0;i<size1[0]*size2[1];i++) out[i]= zmuls(in1[i],DoubleComplex(in2[i],0));}
 
-#define d2z2OpDotStarc2(in1,size1,in2,size2,out)		{int i;\
-										for(int i=0;i<size[0]*size[1];i++) out[i]= zmuls(DoubleComplex(in1[i],0),in2[i],out)}
+#define d2z2OpDotStarz2(in1,size1,in2,size2,out)		{int i=0;\
+										for(i=0;i<size1[0]*size2[1];i++) out[i]= zmuls(DoubleComplex(in1[i],0),in2[i]);}
 
 
 #endif /* !__INT_OPDOTSTAR_H__ */
