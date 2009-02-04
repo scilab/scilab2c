3d1111ee5332b2402ac77d93ed0535231d3389ef
torset <torset@34710fda-3622-0410-992e-dbbbfc6a01d4>
2009-02-04 14:18:00 +0000

    Add MatrixPow directory and files

---
+++ trunk/scilab2c/src/matrixOperations/powm/testDoublePowm.c
@@ -0,0 +1,49 @@
+/*
+ *  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ *  Copyright (C) 2008 - INRIA - Arnaud TORSET
+ *
+ *  This file must be used under the terms of the CeCILL.
+ *  This source file is licensed as described in the file COPYING, which
+ *  you should have received as part of this distribution.  The terms
+ *  are also available at
+ *  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
+
+#include "matrixPow.h"
+#include <stdio.h>
+
+
+static void dpowmaTest(void){
+	double in[4]={1,5,4,2};
+	double out[4];
+	int i;
+	
+	dpowma(in, 2, 2, out);
+	
+	for (i=0;i<4;i++) printf("out[%d] = %f\n",i,out[i]);
+	
+}
+
+
+static void zpowmaTest(void){
+	double inR[4]={1,5,4,2};
+	double inI[4]={2,1,-1,3};
+	doubleComplex *in,out[4];
+	int i;
+	
+	in=malloc((uint)4*sizeof(doubleComplex));
+	in=DoubleMatrixComplex(inR,inI,4);
+	zpowma(in, 2, DoubleComplex(2,0), out);
+	
+	for (i=0;i<4;i++) printf("out[%d] = %f+%f*i\n",i,zreals(out[i]),zimags(out[i]));
+	
+}
+
+
+
+int main(void){
+	dpowmaTest();zpowmaTest();
+	return 0;
+}
+	
