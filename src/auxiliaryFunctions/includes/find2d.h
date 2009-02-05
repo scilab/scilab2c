7e7df6ec419bfcb7d5dc97dc4af973b6ea5c2c09
torset <torset@34710fda-3622-0410-992e-dbbbfc6a01d4>
2009-02-05 12:53:51 +0000

    Adapt find2d for generation

--- src/auxiliaryFunctions/includes/find2d.h
+++ src/auxiliaryFunctions/includes/find2d.h
@@ -19,20 +19,21 @@
 /*
 ** \brief Float Find function
 */
-void sfind2da(float* x, int rows, int columns, float** out1, float** out2);
+void sfind2da(float* x, int rows, int columns, float* out1, int* sizeOut1, float* out2, int* sizeOut2);
 
 /*
 ** \brief Double Find function
 */
-void dfind2da(double*x, int rows, int columns, double** out1, double** out2);
+void dfind2da(double* x, int rows, int columns, double* out1, int* sizeOut1, double* out2, int* sizeOut2);
 
 /*
 ** \brief Float Complex Find function
 */
-void cfind2da(floatComplex* z, int rows, int columns, float** out1, float** out2);
+void cfind2da(floatComplex* z, int rows, int columns, float* out1, int* sizeOut1, float* out2, int* sizeOut2);
 
 /*
 ** \brief Double Complex Find function
 */
-void zfind2da(doubleComplex* z, int rows, int columns, double** out1, double** out2);
+void zfind2da(doubleComplex* z, int rows, int columns, double* out1, int* sizeOut1, double* out2, int* sizeOut2);
+
 #endif /* !__FIND2D_H__ */
