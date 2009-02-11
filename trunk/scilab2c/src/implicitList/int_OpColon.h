331f8649f8bd72b00cd678ad09130d6008082cfd
torset <torset@34710fda-3622-0410-992e-dbbbfc6a01d4>
2009-02-11 15:12:18 +0000

    Add implicitlist interface

---
+++ trunk/scilab2c/src/implicitList/int_OpColon.h
@@ -0,0 +1,56 @@
+/*
+ * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ * Copyright (C) 2008-2008 - INRIA - Bruno JOFRET
+ * 
+ * This file must be used under the terms of the CeCILL.
+ * This source file is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at    
+ * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
+
+/* THIS IS AN AUTOMATICALLY GENERATED FILE : DO NOT EDIT BY HAND. */
+
+#ifndef __INT_OPCOLON_H__
+#define __INT_OPCOLON_H__
+
+#define s0s0OpColons0(in1,in2)			in1
+
+#define d0d0OpColond0(in1,in2)			in1
+
+#define c0c0OpColonc0(in1,in2)			in1
+
+#define z0z0OpColonz0(in1,in2)			in1
+
+
+
+#define s0s0OpColons2(in1,in2,out)			simplicitLists(in1,1,in2,out)
+
+#define d0d0OpColond2(in1,in2,out)			dimplicitLists(in1,1,in2,out)
+
+#define c0c0OpColonc2(in1,in2,out)			cimplicitLists(in1,1,in2,out)
+
+#define z0z0OpColonz2(in1,in2,out)			zimplicitLists(in1,1,in2,out)
+
+
+
+#define s0s0s0OpColons0(in1,in2,in3)		in1
+
+#define d0d0d0OpColond0(in1,in2,in3)		in1
+
+#define c0c0c0OpColonc0(in1,in2,in3)		in1
+
+#define z0z0z0OpColonz0(in1,in2,in3)		in1
+
+
+
+#define s0s0s0OpColons2(in1,in2,in3,out)		simplicitLists(in1,in2,in3,out)
+
+#define d0d0d0OpColond2(in1,in2,in3,out)		dimplicitLists(in1,in2,in3,out)
+
+#define c0c0c0OpColonc2(in1,in2,in3,out)		cimplicitLists(in1,in2,in3,out)
+
+#define z0z0z0OpColonz2(in1,in2,in3,out)		zimplicitLists(in1,in2,in3,out)
+
+#endif /* !__INT_OPCOLON_H__ */
