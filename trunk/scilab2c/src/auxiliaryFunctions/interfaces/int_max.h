d57a61371cd9e26736f29f6a827419fbcd1ccfc8
torset <torset@34710fda-3622-0410-992e-dbbbfc6a01d4>
2009-02-17 15:34:23 +0000

    add max function for arrays

---
+++ trunk/scilab2c/src/auxiliaryFunctions/interfaces/int_max.h
@@ -0,0 +1,26 @@
+/*
+ * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ * Copyright (C) 2008-2008 - INRIA - Bruno JOFRET
+ * 
+ * This file must be used under the terms of the CeCILL.
+ * This source file is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at    
+ * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
+
+/* THIS IS AN AUTOMATICALLY GENERATED FILE : DO NOT EDIT BY HAND. */
+
+#ifndef __INT_MAX_H__
+#define __INT_MAX_H__
+
+#define s0s0maxs0(in1,in2)			max(in1,in2)
+
+#define d0d0maxd0(in1,in2)			max(in1,in2)
+
+#define s2s2maxs2(in,size,out)		maxa(in1, size[0]*size[1], in2, size[0]*size[1], out)
+
+#define d2d2maxd2(in,size,out)		maxa(in1, size[0]*size[1], in2, size[0]*size[1], out)
+
+#endif /* !__INT_MAX_H__ */
