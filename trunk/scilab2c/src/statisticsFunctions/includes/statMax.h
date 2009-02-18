d493170eaddde070bceacde69804886820e615f4
torset <torset@34710fda-3622-0410-992e-dbbbfc6a01d4>
2009-02-18 09:48:12 +0000

    Add min and max in statisticsFunctions

---
+++ trunk/scilab2c/src/statisticsFunctions/includes/statMax.h
@@ -0,0 +1,54 @@
+/*
+ *  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ *  Copyright (C) 2008-2008 - INRIA - Bruno JOFRET
+ *
+ *  This file must be used under the terms of the CeCILL.
+ *  This source file is licensed as described in the file COPYING, which
+ *  you should have received as part of this distribution.  The terms
+ *  are also available at
+ *  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
+
+#ifndef __STAT_MAX_H__
+#define __STAT_MAX_H__
+
+
+/*
+** \brief Sum of a scalar element, just returns it
+*/
+#define		smaxs(in)				in
+#define		srowmaxs(in)			in
+#define		scolumnmaxs(in)			in
+#define		smatlabmaxs(in)			in
+
+/*
+** \brief Sum of a scalar element, just returns it
+*/
+#define		dmaxs(in)				in
+#define		drowmaxs(in)			in
+#define		dcolumnmaxs(in)			in
+#define		dmatlabmaxs(in)			in
+
+/*
+** \brief Sum of a float array
+** \param in the float array to process
+** \param size, the size of the array
+** \returns the max.
+*/
+float		smaxa(float *in, int size);
+void		srowmaxa(float *in, int lines, int columns, float* out);
+void		scolumnmaxa(float *in, int lines, int columns, float* out);
+
+/*
+** \brief Sum of a double array
+** \param in the double array to process
+** \param size, the size of the array
+** \returns the max.
+*/
+double		dmaxa(double *in, int size);
+void		drowmaxa(double *in, int lines, int columns, double* out);
+void		dcolumnmaxa(double *in, int lines, int columns, double* out);
+
+
+#endif /* !__STAT_MAX_H__ */
---
+++ trunk/scilab2c/src/statisticsFunctions/interfaces/int_statMax.h
@@ -0,0 +1,37 @@
+/*
+ * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ * Copyright (C) 2008-2008 - INRIA - Bruno JOFRET
+ * 
+ * This file must be used under the terms of the CeCILL.
+ * This source file is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at    
+ * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
+
+/* THIS IS AN AUTOMATICALLY GENERATED FILE : DO NOT EDIT BY HAND. */
+
+#ifndef __INT_STATMAX_H__
+#define __INT_STATMAX_H__
+
+#define s0maxs0(in)			smaxs(in)
+
+#define d0maxd0(in)			dmaxs(in)
+
+#define s2maxs2(in,size,out)		smaxa(in, size[0]*size[1], out)
+
+#define d2maxd2(in,size,out)		dmaxa(in, size[0]*size[1], out)
+
+
+
+#define s0s0maxs0(in1,in2)			smaxs(in1)
+
+#define d0d0maxd0(in1,in2)			dmaxs(in1)
+ 
+#define s2s0maxs2(in1,size,in2,out)		(in2==2) ? scolumnmaxa(in1, size[0], size[1], out) : srowmaxa(in1, size[0], size[1], out) 
+
+#define d2d0maxd2(in1,size,in2,out)		(in2==2) ? dcolumnmaxa(in1, size[0], size[1], out) : drowmaxa(in1, size[0], size[1], out) 
+
+
+#endif /* !__INT_STATMAX_H__ */
