aba4bbe4926457d7c780913d17f00a96bee5c85f
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2009-04-22 06:23:45 +0000

    malloc

--- scilab2c/src/signalProcessing/ifft/sifftma.c
+++ scilab2c/src/signalProcessing/ifft/sifftma.c
@@ -1,7 +1,7 @@
 /*
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) 2008 - INRIA - Arnaud Torset
- * 
+ *
  * This file must be used under the terms of the CeCILL.
  * This source file is licensed as described in the file COPYING, which
  * you should have received as part of this distribution.  The terms
@@ -10,6 +10,7 @@
  *
  */
 
+#include <malloc.h>
 #include "ifft.h"
 #include "zeros.h"
 
@@ -19,13 +20,13 @@ void sifftma ( float* in , int rows, int cols, float* out){
 	floatComplex* inCpx;
 	floatComplex* outCpx;
 
-	ZEROS = malloc((uint)(rows*cols)*sizeof(float));	
-	outCpx = malloc((uint)(rows*cols)*sizeof(floatComplex));
-	
+	ZEROS = malloc((unsigned int)(rows*cols)*sizeof(float));
+	outCpx = malloc((unsigned int)(rows*cols)*sizeof(floatComplex));
+
 	szerosa(ZEROS,rows,cols);
 	inCpx=FloatComplexMatrix(in,ZEROS,rows*cols);
-	
+
 	cifftma(inCpx,rows,cols,outCpx);
-	
+
 	creala(outCpx, rows*cols, out);
 }
