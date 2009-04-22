bee7cf3443f40a20557d4aeecd5f6f4c93e9825c
cornet <cornet@34710fda-3622-0410-992e-dbbbfc6a01d4>
2009-04-22 07:38:21 +0000

    add implicitList

--- scilab2c/src/implicitList/implicitList.h
+++ scilab2c/src/implicitList/implicitList.h
@@ -13,8 +13,13 @@
 #ifndef __IMPLICITLIST_H__
 #define __IMPLICITLIST_H__
 
+#include "dynlib_implicitlist.h"
 #include "floatComplex.h"
 #include "doubleComplex.h"
+
+#ifdef  __cplusplus
+extern "C" {
+#endif
 
 /*
 ** Create implicit Lists with scilab syntax
@@ -22,12 +27,16 @@
 ** or start:end <=> start:1:end
 */
 
-void simplicitLists(float start, float step, float end, float *out);
+EXTERN_IMPLIST void simplicitLists(float start, float step, float end, float *out);
 
-void dimplicitLists(double start, double step, double end, double *out);
+EXTERN_IMPLIST void dimplicitLists(double start, double step, double end, double *out);
 
-void cimplicitLists(floatComplex start, floatComplex step, floatComplex end, float *out);
+EXTERN_IMPLIST void cimplicitLists(floatComplex start, floatComplex step, floatComplex end, float *out);
 
-void zimplicitLists(doubleComplex start, doubleComplex step, doubleComplex end, double *out);
+EXTERN_IMPLIST void zimplicitLists(doubleComplex start, doubleComplex step, doubleComplex end, double *out);
+
+#ifdef  __cplusplus
+} /* extern "C" */
+#endif
 
 #endif /* !__IMPLICITLIST_H__ */
