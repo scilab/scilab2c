b320d6a263b4f39763643a736b675fef91642ec2
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2009-09-02 05:31:01 +0000

    Enable automated tests

--- trunk/scilab2c/src/c/elementaryFunctions/cosh/testDoubleCosh.c
+++ trunk/scilab2c/src/c/elementaryFunctions/cosh/testDoubleCosh.c
@@ -446,8 +446,8 @@ void zcoshsTest(void) {
 	for (i=0;i<200;i++){
 		in=DoubleComplex(inR[i],inI[i]);
 		out=zcoshs(in);
-		assert(( (fabs(zreals(out)-resR[i]))/(fabs(zreals(out))) )<3e-16);	
-		assert(( (fabs(zimags(out)-resI[i]))/(fabs(zimags(out))) )<3e-16);			
+		assert( fabs(zreals(out) - resR[i]) < 1e-14);	
+		assert( fabs(zimags(out) - resI[i]) < 1e-14);			
 	}
 }
 
@@ -487,8 +487,8 @@ void zcoshaTest(void) {
 	}
 	zcosha(in,200,out);
 	for (i=0;i<200;i++){
-		assert(( (fabs(zreals(out[i])-resR[i]))/(fabs(zreals(out[i]))) )<3e-16);	
-		assert(( (fabs(zimags(out[i])-resI[i]))/(fabs(zimags(out[i]))) )<3e-16);			
+		assert( fabs(zreals(out[i]) - resR[i]) < 3e-15);	
+		assert( fabs(zimags(out[i]) - resI[i]) < 3e-15);			
 	}
 }
 
