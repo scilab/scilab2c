e3724c71f70b0349950996d30575f155e9deb3a9
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2010-07-16 22:47:53 +0000

    Correct FIND function prototypes

--- src/c/auxiliaryFunctions/find/dfinda.c
+++ src/c/auxiliaryFunctions/find/dfinda.c
@@ -13,21 +13,28 @@
 
 #include "find.h"
 
-void dfinda(double* x, int size ,double *out, int *indiceOut , int max ) {
-  int i = 0;
-  indiceOut[1]=0;
-  
-  /* if out is empty */
-  out[0]=-1;
+void dfinda(double* x, int size, double *out, int max)
+{
+    int i = 0;
+    int j = 0;
 
-  
-  for (i = 0; i < size ; ++i) {
-    /*to avoid useless search if we only want to find the  max first founded value   */
-    if (indiceOut[1] == max  ) return ;
-    if (x[i] != 0) {
-	out[indiceOut[1]] = (double)(i+1);
-	indiceOut[1]++;
+    /* if out is empty */
+    out[0]=-1;
+
+
+    for (i = 0; i < size ; ++i)
+    {
+        /*to avoid useless search if we only want to find the  max first founded value   */
+        if (j == max)
+        {
+            return ;
+        }
+
+        if (x[i] != 0)
+        {
+            out[j] = (double)(i+1);
+            ++j;
+        }
     }
-  }
 }
 
