5dfe9bd80bd49aeb9ac348b3c45b0f5b99d766fb
jofret <jofret@34710fda-3622-0410-992e-dbbbfc6a01d4>
2010-09-01 12:02:11 +0000

    Adding size function with 'c', 'r' or '*' as parameter

--- trunk/scilab2c/src/c/auxiliaryFunctions/includes/size.h
+++ trunk/scilab2c/src/c/auxiliaryFunctions/includes/size.h
@@ -14,82 +14,12 @@
 #define __SIZE_H__
 
 /**
- ** WARNING :
- ** We assume size of arrays are known, so we
- ** use #define to avoid compilation warnings
- ** such as "unused parameter"
- **/
-
-/**
- ** \brief Float Size Scalar function
- ** Determine the size of an array.
- ** \param in : the float array we must determine size.
- ** \param size : the number of elements.
- ** \return the size of in.
- **/
-#define	ssizes(in)		1.0f
-
-/**
- ** \brief Float Size Scalar function
- ** Determine the size of an array.
- ** \param in : the float array we must determine size.
- ** \param size : the number of elements.
- ** \return the size of in.
- **/
-#define	dsizes(in)		1.0
-
-/**
- ** \brief Complex Float Size Scalar function
- ** Determine the size of an array.
- ** \param in : the float array we must determine size.
- ** \param size : the number of elements.
- ** \return the size of in.
- **/
-#define	csizes(in)		1.0f
-
-/**
- ** \brief Complex Double Size Array function
- ** Determine the size of an array.
- ** \param in : the float array we must determine size.
- ** \param size : the number of elements.
- ** \return the size of in.
- **/
-#define	zsizes(in)		1.0
-
-/**
- ** \brief Float Size Array function
- ** Determine the size of an array.
- ** \param in : the float array we must determine size.
- ** \param size : the number of elements.
- ** \return the size of in.
- **/
-#define	ssizea(in, size)	size
-
-/**
  ** \brief Double Size Array function
  ** Determine the size of an array.
  ** \param in : the float array we must determine size.
  ** \param size : the number of elements.
  ** \return the size of in.
  **/
-#define	dsizea(in, size)	size
-
-/**
- ** \brief Complex Float Size Array function
- ** Determine the size of an array.
- ** \param in : the float array we must determine size.
- ** \param size : the number of elements.
- ** \return the size of in.
- **/
-#define	csizea(in, size)	size
-
-/**
- ** \brief Complex Double Size Array function
- ** Determine the size of an array.
- ** \param in : the float array we must determine size.
- ** \param size : the number of elements.
- ** \return the size of in.
- **/
-#define	zsizea(in, size)	size
+double dallsizea(int *size, char *select);
 
 #endif /* !__SIZE_H__ */
