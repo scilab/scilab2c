3f0575546e66458cb437c9c333c44c227e58b9bc
imushir <i_mushir@yahoo.com>
2015-11-27 15:43:45 +0530

    reference for u8 u16 i8 i16 for different combination added

--- src/c/matrixOperations/interfaces/int_transpose.h
+++ src/c/matrixOperations/interfaces/int_transpose.h
@@ -23,6 +23,14 @@
 
 #define z0transposez0(in)			in
 
+#define u80transposeu80(in)			(uint8)in
+
+#define u160transposeu160(in)			(uint16)in
+
+#define i80transposei80(in)			(int8)in
+
+#define i160transposei160(in)			(int16)in
+
 #define s2transposes2(in,size,out)		stransposea(in, size[0], size[1], out)
 
 #define d2transposed2(in,size,out)		dtransposea(in, size[0], size[1], out)
@@ -30,5 +38,14 @@
 #define c2transposec2(in,size,out)		ctransposea(in, size[0], size[1], out)
 
 #define z2transposez2(in,size,out)		ztransposea(in, size[0], size[1], out)
+
+
+#define u82transposeu82(in,size,out)		u8transposea(in, size[0], size[1], out)
+
+#define u162transposeu162(in,size,out)		u16transposea(in, size[0], size[1], out)
+
+#define i82transposei82(in,size,out)	        i8transposea(in, size[0], size[1], out)
+
+#define i162transposei162(in,size,out)		i16transposea(in, size[0], size[1], out)
 
 #endif /* !__INT_TRANSPOSE_H__ */
