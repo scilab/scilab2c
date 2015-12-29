7382a75d68141d72562f219a839543c9c6fc83aa
imushir <i_mushir@yahoo.com>
2015-12-29 12:21:00 +0530

    added support for magnitude

---
+++ src/c/matrixOperations/magnitude/i16magns.c
@@ -0,0 +1,13 @@
+
+/* SCilab2C FOSSEE IIT BOMBAY */
+
+
+
+#include "matrixMagnitude.h"
+#include "abs.h"
+
+int16 i16magns(int16 in){
+	return i16abss(in);
+}
+
+
---
+++ src/c/matrixOperations/magnitude/i8magns.c
@@ -0,0 +1,13 @@
+
+/* SCilab2C FOSSEE IIT BOMBAY */
+
+
+
+#include "matrixMagnitude.h"
+#include "abs.h"
+
+int8 i8magns(int8 in){
+	return i8abss(in);
+}
+
+
